a8e0700a2c3efb86247b28d97f885c663a127c73
Ozzy Gonzalez <ozzy.gonzalez@gmail.com>
2018-08-16 20:54:24 -0500

    webpack almost working... No bootstrap though

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
-    protect_from_forgery
+    protect_from_forgery with: :exception
     prepend_view_path Rails.root.join("frontend")
 end
--- frontend/src/application.js
+++ frontend/src/application.js
@@ -1,4 +1,5 @@
 import "./styles/style.css";
+// import "bootstrap/dist/css/bootstrap.min.css";  
 
 console.log("hello alyssa");
 console.log("something");

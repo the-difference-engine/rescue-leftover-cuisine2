9f2f315d927fdf136c169170c358222c0051a7ee
Jason Souza <jsouzadev@gmail.com>
2018-08-22 17:12:49 -0500

    reverting webpack to work with rails assets

--- db/schema.rb
+++ db/schema.rb
@@ -39,7 +39,6 @@ ActiveRecord::Schema.define(version: 2018_08_07_014553) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "user_name", null: false, unique: true 
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
--- frontend/packs/application.js
+++ frontend/packs/application.js
@@ -1,4 +1,4 @@
-import "./styles/style.css";
+import "./application.css";
 
 console.log("hello alyssa");
 console.log("something");

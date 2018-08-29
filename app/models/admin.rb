ce74b9d15dab197c3862ed606988e79b8bf276ef
Tom Murphy <tom.louis.murphy@gmail.com>
2018-08-29 16:25:36 +0000

    added unit and feature specs for admin work

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -3,4 +3,5 @@ class Admin < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  validates	:email, uniqueness: true
 end

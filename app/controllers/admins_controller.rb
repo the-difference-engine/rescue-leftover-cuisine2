10bd6d7aaebb9a2ca98a08b4a4f05857c853abe6
Tom Murphy <tom.louis.murphy@gmail.com>
2018-08-31 01:38:29 +0000

    admins can now click through on edit page to a separate page to change their password

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -7,4 +7,26 @@ class AdminsController < ApplicationController
 		def passwordchange
 			@admin = Admin.find(params[:admin_id])
 		end
+
+		private
+
+		def resource_name
+			:admin
+		end
+		helper_method :resource_name
+
+		def resource
+			@resource ||= Admin.new
+		end
+		helper_method :resource
+
+		def devise_mapping
+			@devise_mapping ||= Devise.mappings[:admin]
+		end
+		helper_method :devise_mapping
+
+		def resource_class
+			Admin
+		end
+		helper_method :resource_class
 end

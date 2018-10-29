5b96993eaff002bffd76f697d55376d8ef2f10aa
Zach Garwood <zachgarwood@gmail.com>
2018-10-29 18:40:24 -0500

    Created demo of how to call API from component
    Also, removed unnecessary configs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,28 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      message: 'Loading...'
+    }
+  }
+  componentDidMount() {
+    axios.get('/api/message.json')
+    .then(response => {
+      this.setState({ message: response.data.message })
+    })
+    .catch(console.error)
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <p>{this.state.message}</p>
         </header>
       </div>
     );

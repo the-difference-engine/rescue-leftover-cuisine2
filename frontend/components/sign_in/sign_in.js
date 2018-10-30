8d008ac6a34a934698b5e15f58265ea52b7ec67e
nycarts <jdalgety@gmail.com>
2018-10-29 19:21:56 -0500

    Initial build: added bootstrap to head, attempting to build & link sign-in component

---
+++ frontend/components/sign_in/sign_in.js
@@ -0,0 +1,10 @@
+const signIn = () => {
+
+  <p>I AM A JEDI!</p>
+
+}
+
+ReactDOM.render( <
+  App / > , // self-contained element, so can use self-closing tag
+  document.getElementById('root')
+);

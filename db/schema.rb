963e9b9d49cb1e9257f640c5154c11adc75ea92f
Tom Murphy <tom.louis.murphy@gmail.com>
2018-11-01 00:47:24 +0000

    redesigned schema to fit STI user/admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,34 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_04_002029) do
+ActiveRecord::Schema.define(version: 2018_11_01_003438) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "admins", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.inet "current_sign_in_ip"
-    t.inet "last_sign_in_ip"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.boolean "super_admin"
-    t.string "user_name"
-    t.string "first_name"
-    t.string "last_name"
-    t.string "profile_photo"
-    t.string "interests"
-    t.boolean "suspended_status"
-    t.index ["email"], name: "index_admins_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
-  end
 
   create_table "comments", force: :cascade do |t|
     t.string "name"
@@ -85,6 +61,8 @@ ActiveRecord::Schema.define(version: 2018_10_04_002029) do
     t.string "profile_photo"
     t.string "interests"
     t.boolean "suspended_status"
+    t.boolean "admin_status"
+    t.boolean "superadmin_status"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true

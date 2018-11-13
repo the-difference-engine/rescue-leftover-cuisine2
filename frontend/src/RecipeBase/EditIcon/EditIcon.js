90d295eb24f44d02ce6b0f63ae7ee433a244f653
Tom Murphy <tom.louis.murphy@gmail.com>
2018-11-12 20:49:40 -0600

    edit icon in progress

--- frontend/src/RecipeBase/EditIcon/EditIcon.js
+++ frontend/src/RecipeBase/EditIcon/EditIcon.js
@@ -5,6 +5,16 @@ export class EditIcon extends Component {
 	render() {
 		return (
 			<div id="edit-icon-container">
+				<div id="pencil-icon-x-1">
+				</div>
+				<div id="pencil-icon-x-2">
+				</div>
+				<div id="pencil-icon-x-3">
+				</div>
+				<div id="pencil-icon-x-4">
+				</div>
+				<div id="pencil-icon-x-5">
+				</div>
 				<span id="edit-icon-text">EDIT</span>
 			</div>
 		);

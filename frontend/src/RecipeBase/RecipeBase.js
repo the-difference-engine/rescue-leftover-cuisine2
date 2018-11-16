00941c326ea620cb2e46dd6e6c5e88e5709dc93d
Tom Murphy <tom.louis.murphy@gmail.com>
2018-11-16 02:51:54 +0000

    moved surplus components into RecipeBase

--- frontend/src/RecipeBase/RecipeBase.js
+++ frontend/src/RecipeBase/RecipeBase.js
@@ -3,8 +3,6 @@ import './RecipeBase.css';
 import {MainImage} from './MainImage/MainImage.js';
 import {SpecsBar} from '../SpecsBar/SpecsBar.js';
 import {TagsBar} from '../TagsBar/TagsBar.js';
-import {MainBody} from './MainBody/MainBody.js';
-import {Directions} from './Directions/Directions.js';
 import {CommentSection} from './CommentSection/CommentSection.js';
 import {DeleteIcon} from './DeleteIcon/DeleteIcon.js';
 import {EditIcon} from './EditIcon/EditIcon.js';
@@ -14,7 +12,8 @@ class RecipeBase extends Component {
   constructor() {
     super();
     this.state = {
-      ingredients: ['1 (8 ounce) package elbow macaroni', '2/3 cup rice bran oil, divided', '2 cups unsweetened soy milk', '1 cup nutritional yeast', '4 cloves garlic, minced', '2 teaspoons yellow mustard', '1 teaspoon ground paprika']
+      ingredients: ['1 (8 ounce) package elbow macaroni', '2/3 cup rice bran oil, divided', '2 cups unsweetened soy milk', '1 cup nutritional yeast', '4 cloves garlic, minced', '2 teaspoons yellow mustard', '1 teaspoon ground paprika'],
+      directions: ['Preheat', 'Bring', 'Combine', 'Pour', 'Bake']
     }
   }
   render() {
@@ -29,7 +28,10 @@ class RecipeBase extends Component {
         <MainImage />
         <SpecsBar />
         <TagsBar />
-        <MainBody />
+        <div className="row" id="blurb-title">
+          <h3 id="recipe-title" className="col-sm-6 offset-sm-3">Easy Creamy Vegan Macaroni and Cheese</h3>
+          <p id="recipe-descrip" className="col-sm-8 offset-sm-2">Although this does not taste like the traditional mac 'n cheese recipes most of us grew up with, it will satisfy your comfort food craving while helping you avoid preservatives, dyes, meat, and dairy. Stir in your favorite veggies and/or meatless filler and you've got yourself a satisfying meal. This recipe is very versatile; tweak ingredients to your liking!</p>
+        </div>
         <div className="separator col-sm-10 offset-sm-1"></div>
         <div id="ingredient-list">
           <h4 id="ingredient-title">Ingredients</h4>
@@ -46,7 +48,20 @@ class RecipeBase extends Component {
           </ul>
         </div>
         <div className="separator col-sm-10 offset-sm-1"></div>
-        <Directions />
+        <div id="direction-container" className="col-sm-10 offset-sm-1">
+          <h4 id="direction-title">Directions</h4>
+          <ul>
+            {this.state.directions.map((direction, index) => {
+              return (
+                <div className="single-direction">
+                  <h1 className="direction-index">{index + 1}</h1>
+                  <p className="direction-action">{direction}</p>
+                  <br/>
+                </div>
+              )
+            })}
+          </ul>
+        </div>
         <CommentSection />
       </div>
     );

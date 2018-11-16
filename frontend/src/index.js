2ae702157daac015ae880fcbd68a7c2f326666a7
Tom Murphy <tom.louis.murphy@gmail.com>
2018-11-15 20:21:12 -0600

    preparing for merge into master

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import App from './App.js';
 import RecipeBase from './RecipeBase/RecipeBase.js';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<RecipeBase />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

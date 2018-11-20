b18095240dbb46b32a4257088b2117142d35a88e
nycarts <jdalgety@gmail.com>
2018-11-19 20:19:30 -0600

    added meal type button to recipe card; will link to recipe tags once API built

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,16 @@ import React, { Component } from 'react';
 // import './App.css';
 import SignIn from './components/sign_in/sign_in';
 import SignUp from './components/sign_up/sign_up';
+import RecipeCard from './components/recipe_card/recipe_card';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header"></header>
-          <SignUp />
-          <SignIn />
+        {/* <SignUp />
+        <SignIn /> */}
+        <RecipeCard />
       </div>
     );
   }
--- frontend/src/components/recipe_card/recipe_card.js
+++ frontend/src/components/recipe_card/recipe_card.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './recipe_card.css';
+import PunkCat from './../../assets/punk_cat.jpg';
 
 const RecipeCard = () => {
   return (
     <div className="card-container">
       <div className="card">
-        <img className="card-img-top" src="..." alt="Card image cap" />
-        {/* overlay meal type on graphic */}
+        <div className="image-holder">
+          <img className="card-img-top" src={PunkCat} alt="cat in punk rock vest" />
+          <button>DINNER</button>
+        </div>
         <div className="card-body">
           <div className="card-title">I am a recipe title!</div>
           <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>

81bfc5ea17a6268446958c366b8b2066399d0230
Evan Grillo <evan.grillo@outlook.com>
2018-11-20 15:34:17 -0600

    router set up with example

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Router, Route, Link, Switch } from 'react-router-dom'
 import MainFooter from './components/Footer/Footer.js';
+import LoginPage from './components/LoginPage/LoginPage.js';
 import axios from 'axios';
 import history from './history';
 
@@ -22,17 +23,29 @@ class App extends Component {
     return (
       <Router history={history}> 
         <div>
-            <h1 className="jumbotron text-center">{this.state.message}</h1>
-            <button className="btn">
-              <Link to="/footer">Render Footer</Link>
-            </button>
-            <hr></hr>
-            <button className="btn">
-              <Link to="/">Now make it go away with a new route</Link>
-            </button>
 
+          <div className="wrapper">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+              <Link to="/"><h1 className="navbar-brand">{this.state.message}</h1></Link>
+              <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div className="navbar-nav">
+                  <Link to="/footer" className="nav-item nav-link active">Render Footer <span className="sr-only">(current)</span></Link>
+                  <Link to="/login" className="nav-item nav-link active">Render Login <span className="sr-only">(current)</span></Link>
+                  <Link to="/" className="nav-item nav-link active">Remove Components<span className="sr-only">(current)</span></Link>
+                </div>
+              </div>
+            </nav>
+          </div>
+
+          <div className="wrapper">
             <Route exact path="/" />
             <Route path="/footer" component={MainFooter} />
+            <Route path="/login" component={LoginPage} />
+          </div>
+
         </div>
       </Router>
     

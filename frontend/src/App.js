d83f081c58c3df05860ce00f0c381cfff54a994e
Evan Grillo <evan.grillo@outlook.com>
2018-11-21 15:56:56 -0600

    tweaks

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -25,22 +25,22 @@ class App extends Component {
         <div>
 
           <div className="wrapper">
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
               <Link to="/"><h1 className="navbar-brand">{this.state.message}</h1></Link>
               <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
               </button>
               <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div className="navbar-nav">
-                  <Link to="/footer" className="nav-item nav-link active">Render Footer <span className="sr-only">(current)</span></Link>
-                  <Link to="/login" className="nav-item nav-link active">Render Login <span className="sr-only">(current)</span></Link>
-                  <Link to="/" className="nav-item nav-link active">Remove Components<span className="sr-only">(current)</span></Link>
+                  <Link to="/footer" className="nav-item nav-link active">Render Footer </Link>
+                  <Link to="/login" className="nav-item nav-link active">Render Login </Link>
+                  <Link to="/" className="nav-item nav-link active">Remove Components</Link>
                 </div>
               </div>
             </nav>
           </div>
 
-          <div className="">
+          <div className="wrapper">
             <Route exact path="/" />
             <Route path="/footer" component={MainFooter} />
             <Route path="/login" component={LoginPage} />

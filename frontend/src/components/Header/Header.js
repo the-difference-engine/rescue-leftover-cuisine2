927e6944c9bb284b49d860fbc0e22cf4ff6c1253
ryandwalters <ryandwalters@gmail.com>
2018-12-03 19:53:23 -0600

    Header js, css, and logo added for frontend homepage view

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import "./Header.css"
+
 
 class Header extends Component {
     constructor(props) {
@@ -10,54 +12,35 @@ class Header extends Component {
 
     render() {
 
-        let styles = {
-            logo: {
-                width: "80%",
-                paddingTop: "10px",
-                paddingBottom: "10px"
-            },
-
-            centerButtonSection: {
-                textAlign: "center"
-            },
-
-            donateButton: {
-                paddingLeft: "60px",
-                paddingRight: "60px"
-            }
-        };
 
         return (
 
             <div className="container-fluid">
                 <div className="row">
                     <nav className="navbar navbar-expand-lg navbar-white bg-white navbar-fill fixed-top">
-
                         <div className="col-md-4">
-                            <div className="navbar-brand w-25" href="/">
-                        
-                                <img src={require('./RLC_LOGO.png')} alt="RLC Logo" style={styles.logo} />
+                            <div className="navbar-brand" href="/">
+                                <img src={require('./RLC_LOGO.png')} alt="RLC Logo" className="logo"/>
                             </div>
                         </div>
                         <div className="col-md-2"></div>
                         <div className="col-md-6">
-                            <span style={styles.centerButtonSection}>
+                            <span className="centerButtonSection">
                                 <ul className="nav navbar-nav navbar-right btn-group btn-group-lg btn-block">
-                                    <div className="btn-group mr-3" role="group" aria-label="browse recipies" style={styles.buttonSize}>
-                                        <li><button type="button" className="btn-lg">Browse Recipies</button></li>
+                                    <div className="btn-group mr-3" role="group" aria-label="browse recipies">
+                                        <li><button type="button" className="btn btn-Light btn-lg">Browse Recipies</button></li>
                                     </div>
-                                    <div className="btn-group mr-3" role="group" aria-label="log in or sign up" style={styles.buttonSize}>
+                                    <div className="btn-group mr-3 buttonSize" role="group" aria-label="log in or sign up">
                                         <li><button type="button" className="btn btn-outline-primary btn-lg">Log In or Sign Up</button></li>
                                     </div>
                                     <div className="btn-group mr-3" role="group" aria-label="donate" >
-                                        <li><button type="button" className="btn btn-warning btn-lg" style={styles.donateButton}>Donate</button></li>
+                                        <li><button type="button" className="btn btn-warning btn-lg donateButton bold">Donate</button></li>
                                     </div>
                                 </ul>
                             </span>
                         </div>
                     </nav>
                 </div>
-
             </div>
         )
     }

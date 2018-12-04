94eb3d141c9d6034dca6ba5de0c9525d97ec3eaa
nycarts <jdalgety@gmail.com>
2018-12-03 19:29:15 -0600

    resolved merge conflicts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,46 +10,46 @@ import axios from 'axios';
 import history from './history';
 import UsersAll from './components/AdminControls/UsersAll.js'
 import User from './components/AdminControls/User.js'
- // import RecipeList from './components/recipe_card/recipe_card';
- // import PunkCat from './assets/punk_cat.jpg';
- // import ChuckNorris from './assets/chuck_norris.jpeg';
- // import SnapeVoice from './assets/snape_voice.jpg';
- // import CatReligion from './assets/cat_religion.jpg';
- 
- // const recipes = [
- //   {
- //     id: 1,
- //     name: PunkCat,
- //     alternate: 'Cat in Punk Rock Vest',
- //     meal: 'DINNER',
- //     title: "I am the Cat's title!",
- //     text: "I am the Cat's text!",
- //   },
- //   {
- //     id: 2,
- //     name: ChuckNorris,
- //     alternate: 'Chuck Norris Quote',
- //     meal: 'BREAKFAST',
- //     title: "I am Chuck's title!",
- //     text: "I am Chuck's text!",
- //   },
- //   {
- //     id: 3,
- //     name: SnapeVoice,
- //     alternate: 'Snape v Potter',
- //     meal: 'BRUNCH',
- //     title: "I am Snape's title!",
- //     text: "I am the Snape's text!",
- //   },
- //   {
- //     id: 4,
- //     name: CatReligion,
- //     alternate: 'Cat Religion',
- //     meal: 'LUNCH',
- //     title: "I am the Cat Religion's title!",
- //     text: "I am the Cat Religion's text!",
- //   }
- // ]
+// import RecipeList from './components/recipe_card/recipe_card';
+// import PunkCat from './assets/punk_cat.jpg';
+// import ChuckNorris from './assets/chuck_norris.jpeg';
+// import SnapeVoice from './assets/snape_voice.jpg';
+// import CatReligion from './assets/cat_religion.jpg';
+
+// const recipes = [
+//   {
+//     id: 1,
+//     name: PunkCat,
+//     alternate: 'Cat in Punk Rock Vest',
+//     meal: 'DINNER',
+//     title: "I am the Cat's title!",
+//     text: "I am the Cat's text!",
+//   },
+//   {
+//     id: 2,
+//     name: ChuckNorris,
+//     alternate: 'Chuck Norris Quote',
+//     meal: 'BREAKFAST',
+//     title: "I am Chuck's title!",
+//     text: "I am Chuck's text!",
+//   },
+//   {
+//     id: 3,
+//     name: SnapeVoice,
+//     alternate: 'Snape v Potter',
+//     meal: 'BRUNCH',
+//     title: "I am Snape's title!",
+//     text: "I am the Snape's text!",
+//   },
+//   {
+//     id: 4,
+//     name: CatReligion,
+//     alternate: 'Cat Religion',
+//     meal: 'LUNCH',
+//     title: "I am the Cat Religion's title!",
+//     text: "I am the Cat Religion's text!",
+//   }
+// ]
  
  class App extends Component {
    constructor(props) {
@@ -67,14 +67,10 @@ import User from './components/AdminControls/User.js'
   }
   render() {
     return (
-    //   <div className="App">
-    //     <header className="App-header"></header>
-    //     <RecipeList recipes={recipes} />
-    //   </div>
-    // );
+
       <div className="App">
         <header className="App-header"></header>
-        <RecipeList recipes={recipes} />
+        {/* <RecipeList recipes={recipes} /> */}
 
       <Router history={history}> 
           <div className="wrapper">

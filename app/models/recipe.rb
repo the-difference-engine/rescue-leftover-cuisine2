4293897e5d277cbbda343cc948e66e2e5df5f692
Evan Grillo <evan.grillo@outlook.com>
2018-12-06 19:09:42 -0600

    plugged in search input dependency

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,6 @@ class Recipe < ActiveRecord::Base
 	validates :title, :ingredients, :directions, :presence => true
 	validates_inclusion_of :difficulty, :in => ["Beginner", "Intermediate", "Advanced"]
 	validates :servings, numericality: {only_integer: true, greater_than_or_equal_to: 1}, :allow_nil => true
+
+	belongs_to :user
 end

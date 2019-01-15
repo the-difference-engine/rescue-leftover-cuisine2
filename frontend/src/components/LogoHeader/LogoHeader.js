1f908c982ca740abaabef9677ccc930d1f295bd4
Evan Grillo <evan.grillo@outlook.com>
2019-01-14 19:43:40 -0600

    created login page & logo header component

--- frontend/src/components/LogoHeader/LogoHeader.js
+++ frontend/src/components/LogoHeader/LogoHeader.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-import "./LoginPage.css"
-import LogoHeader from "./../LogoHeader/LogoHeader.js"
+import "./LogoHeader.css"
 
-class LoginPage extends Component {
+class LogoHeader extends Component {
 
   handleInputChange(event) {
       this.setState({ [event.target.name]: event.target.value });
@@ -12,12 +11,12 @@ class LoginPage extends Component {
   render() {
       return (
         <div className="wrapper">
-          <LogoHeader />
-          <div className="pageHeaderDiv">
+          <div className="pageLogoDiv">
+            <img className="logo" src="https://www.rescuingleftovercuisine.org/assets/RLC_LOGO-900fcd5d7d719b63e43d29ac23775f74f17100194e2690e17833a091b016da85.png" />
           </div>
         </div>
       );
     }
 }
 
-export default LoginPage; 
+export default LogoHeader; 

b05e015c271583704e2db5f30e02cd8c7b069721
Meg Glavey <glaveym@gmail.com>
2019-01-14 18:23:51 -0600

    import into app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-browser-dom';
+import Homepage from './containers/Homepage';
+import LoginPage from './containers/LoginPage';
+import SignUp from './components/SignUp';
+import UsersAll from './components/AdminControls/UsersAll';
+import User from './components/AdminControls/User';
+
 
 class App extends Component {
   render() {
@@ -7,7 +13,7 @@ class App extends Component {
       <BrowserRouter>
         <div>
           <Switch>
-            <Route exact path='/' component={LandingPage}></Route>
+            <Route exact path='/' component={Homepage}></Route>
             <Route path='/login' component={LoginPage}></Route>
             <Route path='/signup' component={SignUp}></Route>
             <Route exact path='/users' history={history} render={(props) => <UsersAll {...props} />}></Route>

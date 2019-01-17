87baa9113b4148c149583261e1d3222afc425cac
Bryan Vanderhoof <github@brynomial.com>
2019-01-16 20:01:21 -0600

    Revert "104 app reorganization"

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_01_003438) do
+ActiveRecord::Schema.define(version: 2018_11_09_013523) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "authentication_tokens", force: :cascade do |t|
+    t.string "body"
+    t.bigint "user_id"
+    t.datetime "last_used_at"
+    t.integer "expires_in"
+    t.string "ip_address"
+    t.string "user_agent"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["body"], name: "index_authentication_tokens_on_body"
+    t.index ["user_id"], name: "index_authentication_tokens_on_user_id"
+  end
 
   create_table "comments", force: :cascade do |t|
     t.string "name"
@@ -68,5 +81,6 @@ ActiveRecord::Schema.define(version: 2018_11_01_003438) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "authentication_tokens", "users"
   add_foreign_key "comments", "recipes"
 end
--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -11,37 +11,39 @@ class Header extends Component {
     }
 
     render() {
+
+
         return (
+
             <div className="container-fluid">
                 <div className="row">
                     <nav className="navbar navbar-expand-lg navbar-white bg-white navbar-fill fixed-top">
                         <div className="col-md-4">
                             <div className="navbar-brand" href="/">
-                                <img src={require('.../assets/RLC_logo.png')} alt="RLC Logo" className="logo"/>
+                                <img src={require('./RLC_LOGO.png')} alt="RLC Logo" className="logo"/>
                             </div>
                         </div>
-                        <div className="col-md-2">
-                            <div className="col-md-6">
-                                <span className="centerButtonSection">
-                                    <ul className="nav navbar-nav navbar-right btn-group btn-group-lg btn-block">
-                                        <div className="btn-group mr-3" role="group" aria-label="browse recipies">
-                                            <li><button type="button" className="btn btn-Light btn-lg">Browse Recipies</button></li>
-                                        </div>
-                                        <div className="btn-group mr-3 buttonSize" role="group" aria-label="log in or sign up">
-                                            <li><button type="button" className="btn btn-outline-primary btn-lg">Log In or Sign Up</button></li>
-                                        </div>
-                                        <div className="btn-group mr-3" role="group" aria-label="donate" >
-                                            <li><button type="button" className="btn btn-warning btn-lg donateButton bold">Donate</button></li>
-                                        </div>
-                                    </ul>
-                                </span>
-                            </div>
+                        <div className="col-md-2"></div>
+                        <div className="col-md-6">
+                            <span className="centerButtonSection">
+                                <ul className="nav navbar-nav navbar-right btn-group btn-group-lg btn-block">
+                                    <div className="btn-group mr-3" role="group" aria-label="browse recipies">
+                                        <li><button type="button" className="btn btn-Light btn-lg">Browse Recipies</button></li>
+                                    </div>
+                                    <div className="btn-group mr-3 buttonSize" role="group" aria-label="log in or sign up">
+                                        <li><button type="button" className="btn btn-outline-primary btn-lg">Log In or Sign Up</button></li>
+                                    </div>
+                                    <div className="btn-group mr-3" role="group" aria-label="donate" >
+                                        <li><button type="button" className="btn btn-warning btn-lg donateButton bold">Donate</button></li>
+                                    </div>
+                                </ul>
+                            </span>
                         </div>
                     </nav>
                 </div>
             </div>
         )
     }
- }
+}
 
 export default Header;

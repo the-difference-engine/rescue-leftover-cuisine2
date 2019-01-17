d53ea17f039071dcf82be0f67357e7876e8b0447
Meg Glavey <glaveym@gmail.com>
2019-01-16 18:19:02 -0600

    adding bryans requested changes from pr review

--- frontend/src/containers/Homepage/Homepage.js
+++ frontend/src/containers/Homepage/Homepage.js
@@ -25,19 +25,6 @@ class Homepage extends Component {
     render() {
       return (
         <div className="wrapper">
-          <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
-            <Link to="/"><h1 className="navbar-brand">{this.state.message}</h1></Link>
-              <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-              </button>
-              <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div className="navbar-nav">
-                  <Link to="/footer" className="nav-item nav-link active">Render Footer </Link>
-                  <Link to="/login" className="nav-item nav-link active">Render Login </Link>
-                  <Link to="/" className="nav-item nav-link active">Remove Components</Link>
-                </div>
-              </div>
-          </nav>
           <Header/>
           <MainSearch/>
           <Footer/>

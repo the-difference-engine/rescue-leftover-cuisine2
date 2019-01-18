986f99e0633b83dd287609747bb8d4985402e165
Meg Glavey <glaveym@gmail.com>
2019-01-17 20:24:02 -0600

    added dotenv file

--- frontend/src/containers/Homepage/Homepage.js
+++ frontend/src/containers/Homepage/Homepage.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import Header from 'src/components/Header/Header.js'
-import Footer from 'src/components/Footer/Footer.js';
-import MainSearch from 'src/components/MainSearch/MainSearch';
+import Header from 'components/Header/Header.js'
+import Footer from 'components/Footer/Footer.js';
+import MainSearch from 'components/MainSearch/MainSearch';
 
 
 class Homepage extends Component {

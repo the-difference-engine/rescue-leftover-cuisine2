5fe46ff20a8030cd086e0f7560c9209af757643f
Meg Glavey <glaveym@gmail.com>
2019-01-17 19:33:19 -0600

    adding circleci changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
-import Homepage from './containers/Homepage';
-import LoginPage from './containers/LoginPage';
-import SignUp from './components/SignUp';
-import UsersAll from './components/AdminControls/UsersAll';
-import User from './components/AdminControls/User';
+import Homepage from './containers/Homepage/Homepage.js';
+import LoginPage from './containers/LoginPage/LoginPage.js';
+import SignUp from './components/SignUp/SignUp.js';
+import UsersAll from './components/AdminControls/UsersAll.js';
+import User from './components/AdminControls/User.js';
 import history from './history.js';
 
 

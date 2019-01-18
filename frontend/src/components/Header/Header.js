e15ad328527e5411d81c03d599916efd4ee3df3c
Bryan Vanderhoof <bvanderhoof@heroku.com>
2019-01-18 10:05:33 -0600

    Fix nesting in Header.js

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -20,22 +20,21 @@ class Header extends Component {
                                 <img src={require('../../assets/RLC_logo.png')} alt="RLC Logo" className="logo"/>
                             </div>
                         </div>
-                        <div className="col-md-2">
-                            <div className="col-md-6">
-                                <span className="centerButtonSection">
-                                    <ul className="nav navbar-nav navbar-right btn-group btn-group-lg btn-block">
-                                        <div className="btn-group mr-3" role="group" aria-label="browse recipies">
-                                            <li><button type="button" className="btn btn-Light btn-lg">Browse Recipies</button></li>
-                                        </div>
-                                        <div className="btn-group mr-3 buttonSize" role="group" aria-label="log in or sign up">
-                                            <li><button type="button" className="btn btn-outline-primary btn-lg">Log In or Sign Up</button></li>
-                                        </div>
-                                        <div className="btn-group mr-3" role="group" aria-label="donate" >
-                                            <li><button type="button" className="btn btn-warning btn-lg donateButton bold">Donate</button></li>
-                                        </div>
-                                    </ul>
-                                </span>
-                            </div>
+                        <div className="col-md-2"></div>
+                        <div className="col-md-6">
+                            <span className="centerButtonSection">
+                                <ul className="nav navbar-nav navbar-right btn-group btn-group-lg btn-block">
+                                    <div className="btn-group mr-3" role="group" aria-label="browse recipies">
+                                        <li><button type="button" className="btn btn-Light btn-lg">Browse Recipies</button></li>
+                                    </div>
+                                    <div className="btn-group mr-3 buttonSize" role="group" aria-label="log in or sign up">
+                                        <li><button type="button" className="btn btn-outline-primary btn-lg">Log In or Sign Up</button></li>
+                                    </div>
+                                    <div className="btn-group mr-3" role="group" aria-label="donate" >
+                                        <li><button type="button" className="btn btn-warning btn-lg donateButton bold">Donate</button></li>
+                                    </div>
+                                </ul>
+                            </span>
                         </div>
                     </nav>
                 </div>

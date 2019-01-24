13172c3178e8780e466bb6f6e3d5429b71b0bd06
Evan Grillo <evan.grillo@outlook.com>
2019-01-23 19:35:42 -0600

    circleci errors

--- frontend/src/components/LogoHeader/LogoHeader.js
+++ frontend/src/components/LogoHeader/LogoHeader.js
@@ -12,7 +12,7 @@ class LogoHeader extends Component {
       return (
         <div className="wrapper">
           <div className="pageLogoDiv">
-            <img className="logo" src="https://www.rescuingleftovercuisine.org/assets/RLC_LOGO-900fcd5d7d719b63e43d29ac23775f74f17100194e2690e17833a091b016da85.png" />
+            <img className="logo" src="https://www.rescuingleftovercuisine.org/assets/RLC_LOGO-900fcd5d7d719b63e43d29ac23775f74f17100194e2690e17833a091b016da85.png" alt="" />
           </div>
         </div>
       );

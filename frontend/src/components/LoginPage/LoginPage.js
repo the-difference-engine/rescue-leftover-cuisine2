332db70ae3b232032ebfcab067497bcaa2d89d90
Evan Grillo <evan.grillo@outlook.com>
2019-01-23 19:25:56 -0600

    reimplemented with CSS best practices

---
+++ frontend/src/components/LoginPage/LoginPage.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import "./LoginPage.css"
+import LogoHeader from "./../LogoHeader/LogoHeader.js"
+import SignUp from "./../sign_up/sign_up.js"
+import SignIn from "./../SignIn/SignIn.js"
+
+class LoginPage extends Component {
+
+ handleInputChange(event) {
+      this.setState({ [event.target.name]: event.target.value });
+      console.log("handle change", event.target);
+  }
+
+render() {
+	return (
+		 <div className="wrapper">
+	          <LogoHeader />
+	          <div className="pageHeaderDiv">
+	          </div>
+	          <div className="cardWrapper">
+	            <SignUp />
+	            <SignIn />
+	          </div>
+	        </div>
+	      );
+	    }
+	    
+    }
+
+export default LoginPage;

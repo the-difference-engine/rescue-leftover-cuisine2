f861009192a617916645995cd94883def8e5a1d7
Evan Grillo <evan.grillo@outlook.com>
2019-01-23 19:58:19 -0600

    finalized commit for this branch

--- frontend/src/components/LogoHeader/LogoHeader.js
+++ frontend/src/components/LogoHeader/LogoHeader.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
 import "./LogoHeader.css"
+import Logo from "../../assets/RLC_logo.png";
 
 class LogoHeader extends Component {
-
-  handleInputChange(event) {
-      this.setState({ [event.target.name]: event.target.value });
-      console.log("handle change", event.target);
-  }
 
   render() {
       return (
         <div className="wrapper">
           <div className="pageLogoDiv">
-            <img className="logo" src="https://www.rescuingleftovercuisine.org/assets/RLC_LOGO-900fcd5d7d719b63e43d29ac23775f74f17100194e2690e17833a091b016da85.png" alt="" />
+            <img className="logo" src={Logo} alt="" />
           </div>
         </div>
       );

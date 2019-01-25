fd694ba22dbcae05e24d5d4940f8bfbd6e42a35c
mareann <majgfi@gmail.com>
2019-01-24 20:34:51 -0600

    changes pre master merge

--- db/migrate/20190123120000_create_join_recipe_tags.rb
+++
@@ -1,9 +0,0 @@
-# Create Join Recipe Tags Table
-
-class CreateJoinTableRecipeTags < ActiveRecord::Migration[5.2]
-  def change
-    create_join_table :recipes, :tags do |t| 
-      t.index [ :recipe_id, :tag_id ] 
-    end
-  end
-end

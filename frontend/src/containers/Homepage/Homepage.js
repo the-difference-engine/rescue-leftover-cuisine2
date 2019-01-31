45a1a364cc969a8161d76983e45c2af214051600
Bryan Vanderhoof <bvanderhoof@heroku.com>
2019-01-30 19:27:12 -0600

    Move recipe list and bottom signup components to homepage

--- frontend/src/containers/Homepage/Homepage.js
+++ frontend/src/containers/Homepage/Homepage.js
@@ -2,7 +2,21 @@ import React, { Component } from 'react';
 import Header from '../../components/Header/Header.js'
 import Footer from '../../components/Footer/Footer.js';
 import MainSearch from '../../components/MainSearch/MainSearch';
+import RecipeList from './components/RecipeCard/RecipeCard.js';
+import BottomSignUp from './components/BottomSignUp/BottomSignUp.js'
 
+const recipes = [
+  {
+    photo_url: 'http://placekitten.com/g/325/325',
+    meal: 'BREAKFAST',
+    title: 'Scrambled Eggs',
+    snippet: 'The best scrambled eggs recipe, hands down!',
+    difficulty: 'MEDIUM',
+    duration: 5,
+    servings: 10,
+    id: 1,
+  },
+]
 
 class Homepage extends Component {
     constructor(props) {
@@ -17,6 +31,8 @@ class Homepage extends Component {
         <div className="wrapper">
           <Header/>
           <MainSearch/>
+          <RecipeList recipes={recipes} />
+          <BottomSignUp/>
           <Footer/>
         </div>
       )

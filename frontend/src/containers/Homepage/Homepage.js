c2ddba7b2e8cf05e9c8007eea2eb6cb993182331
Meg Glavey <glaveym@gmail.com>
2019-02-06 19:54:16 -0600

    continued making styling changes

--- frontend/src/containers/Homepage/Homepage.js
+++ frontend/src/containers/Homepage/Homepage.js
@@ -3,7 +3,8 @@ import Header from '../../components/Header/Header.js'
 import Footer from '../../components/Footer/Footer.js';
 import MainSearch from '../../components/MainSearch/MainSearch';
 import RecipeSearchList from '../../components/RecipeSearchList/RecipeSearchList.js';
-import BottomSignUp from '../../components/BottomSignUp/BottomSignUp.js'
+import BottomSignUp from '../../components/BottomSignUp/BottomSignUp.js';
+import './Homepage.css'
 
 class Homepage extends Component {
     constructor(props) {
@@ -15,12 +16,20 @@ class Homepage extends Component {
 
     render() {
       return (
-        <div className="wrapper">
-          <Header/>
-          <MainSearch />
-          <RecipeSearchList />
-          <BottomSignUp/>
-          <Footer/>
+        <div className="homepage container-fluid ">
+          <div className="row">
+            <div className="header">
+              <Header/>
+            </div>
+          </div>
+          <div className="row">
+            <div className="homepageContent">
+              <MainSearch />
+              <RecipeSearchList />
+              <BottomSignUp/>
+              <Footer/>
+            </div>
+          </div>
         </div>
       )
     }

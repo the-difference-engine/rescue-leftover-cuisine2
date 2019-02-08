d37aed6eab63d9fc30c6c7a307b225c036296850
Meg Glavey <glaveym@gmail.com>
2019-02-07 19:54:19 -0600

    commiting most recent changes

--- frontend/src/components/BottomSignUp/BottomSignUp.js
+++ frontend/src/components/BottomSignUp/BottomSignUp.js
@@ -1,29 +1,33 @@
 import React, {Component } from 'react';
 import './BottomSignUp.css'
-import signupImage from "./soup-kitchen.jpg";
+import signupImage from "../../assets/soup-kitchen.jpg";
 
 class BottomSignUp extends Component {
 
   render (){
       return(
-        <div className="bottomContainer container-fluid">
-          <div className="bottomSection row no-gutters">
-              <div className="col-md-6 col-xs-12">
+        <div className="container-fluid containerSection">
+          <div className="bottomSignUp">
+            <div className="row">
+              <div className="col">
                 <img className="signupImage" src={signupImage} alt="sign up"/>
               </div>
-              <div className="col-md-6 col-xs-12 float-right signupSection">
-                <p className="signupText">
-                  <br/>
-                  <span>Sign up to create your recipe<br/></span>
-                  <span> and share them with our<br/></span>
-                  <span> community!<br/></span>
-                  <br/>
-                  <span className="signupButtonSection">
-                    <button type="button" className="btn btn-lg">Sign Up
+              <div className="col">
+                <div className="signupSection">
+                  <p className="signupText">
+                    <br/>
+                    <span>Sign up to create your recipe<br/></span>
+                    <span> and share them with our<br/></span>
+                    <span> community!<br/></span>
+                    <br/>
+                    <span className="signupButtonSection">
+                      <button type="button" className="btn btn-lg">Sign Up
                     </button>
-                  </span>        
-                </p>
+                    </span>        
+                  </p>
+                </div>
               </div>
+            </div>
           </div>
         </div>
       )

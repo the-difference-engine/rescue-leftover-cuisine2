c45760631fa45ca9e2bd310df350049f89baa10e
Katie <katmae83@gmail.com>
2019-02-20 20:04:37 -0600

    aligned form fields with button

--- frontend/src/components/sign_up/sign_up.js
+++ frontend/src/components/sign_up/sign_up.js
@@ -29,12 +29,15 @@ class SignUp extends Component {
           <div className="form-group row">
             <input type="email" id="inputEmail14" className="form-control-lg" placeholder="Email address" required="" autoFocus="" />
             <label htmlFor="inputEmail14"></label>
-  
+          </div>
+          <div className="form-group row">
             <input type={this.state.isPasswordVisible ? "text" : "password"} id="inputPassword4" className="form-control-lg" placeholder="Password" required="" />
             <label htmlFor="inputPassword4"></label>
             <span className={this.state.isPasswordVisible ? "fas fa-eye-slash fa-lg" : "fas fa-eye fa-lg"} onClick={ this.toggleIcon }></span>
           </div>
-          <button className="signUpButton btn btn-lg btn-block" type="submit">Sign Up</button>
+          <div className="form-group row">
+            <button className="signUpButton btn btn-lg btn-block" type="submit">Sign Up</button>
+          </div>
         </form>
       </div>
       

47fee6ea53da216782fa64d56a6c4b2794444e44
Katie <katmae83@gmail.com>
2019-02-21 23:45:34 -0600

    placeholder position and color change on focus

--- frontend/src/components/sign_up/sign_up.js
+++ frontend/src/components/sign_up/sign_up.js
@@ -23,29 +23,33 @@ class SignUp extends Component {
   //   }
   // }
 
-  changePlaceholder = () => {
-    document.getElementById('inputFirstName').placeholder = "First Name";
-  }
-
-  reversePlaceholder = () => {
-    document.getElementById('inputFirstName').placeholder = "First";
-  }
-
   render() {
     return (
       <div class="loginCard">
         <h3 class="loginHeader">Create an Account</h3>
         <form className="form-signUp">
           <div className="form-group row">
-              <input type="text" id="inputFirstName" className="firstName col-md form-control-lg" placeholder="First" required="" autoFocus="" onFocus={this.changePlaceholder} onBlur={this.reversePlaceholder}/>
-              <input type="text" id="inputLastName" className="col-md form-control-lg" placeholder="Last" required="" autoFocus="" />
+              <div className="col">
+                <div className="name row">
+                  <input type="text" id="inputFirstName" className="firstName col-md form-control-lg" required="" autoFocus="" onFocus={this.changePlaceholder} onBlur={this.reversePlaceholder}/>
+                  <label for="inputFirstName">First</label>
+                </div>
+              </div>
+              <div className="col">
+                <div className="name row">
+                  <input type="text" id="inputLastName" className="col-md form-control-lg" required="" autoFocus="" />
+                  <label for="inputLastName">Last</label>
+                </div>
+              </div>
           </div>
 
           <div className="form-group row">
-            <input type="email" id="inputSignUpEmail" className="fullWidth form-control-lg" placeholder="Email" required="" autoFocus="" />
-          </div>
+            <input type="email" id="inputSignUpEmail" className="fullWidth form-control-lg" required="" autoFocus="" />
+            <label for="inputSignUpEmail">Email</label>
+            </div>
           <div className="password form-group row">
-            <input type={this.state.isPasswordVisible ? "text" : "password"} id="inputSignUpPassword" className="fullWidth form-control-lg" placeholder="Password" required=""/>
+            <input type={this.state.isPasswordVisible ? "text" : "password"} id="inputSignUpPassword" className="fullWidth form-control-lg" required=""/>
+            <label for="inputsignUpPassword">Password</label>
             <span className={this.state.isPasswordVisible ? "fas fa-eye-slash fa-lg" : "fas fa-eye fa-lg"} onClick={ this.toggleIcon }></span>
           </div>
           <div className="form-group row">

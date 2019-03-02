41493b3eb4bbeb1dd92e96633b4770fb79f355f1
Katie <katmae83@gmail.com>
2019-03-02 14:39:42 -0600

    styling onBlur and onFocus

--- frontend/src/components/SignUp/SignUp.js
+++ frontend/src/components/SignUp/SignUp.js
@@ -23,17 +23,26 @@ class SignUp extends Component {
   //   }
   // }
  
-  handleChange = (event) => {
-      let info = event.target.value;
-      if (info.length > 0) {
-        this.setState({
-          textPresent: true
-        })
-      }else {
-        this.setState({
-          textPresent: false
-        })
-      }
+  // handleChange = (event) => {
+  //     let info = event.target.value;
+  //     if (info.length > 0) {
+  //       this.setState({
+  //         textPresent: true
+  //       })
+  //     }else {
+  //       this.setState({
+  //         textPresent: false
+  //       })
+  //     }
+  // }
+
+  togglePlaceholder = (placeholder) => {
+    console.log(placeholder)
+  
+  }
+
+  addPlaceholder = () => {
+    "Email";
   }
 
   render() {
@@ -44,24 +53,28 @@ class SignUp extends Component {
           <div className="form-group row">
               <div className="name col">
                 <div className="name row">
-                  <input type="text" id="inputFirstName" className="firstName col-md form-control-lg" required="" autoFocus="" onChange={(event) => this.handleChange(event)}/>
+                  <input type="text" id="inputFirstName" className="firstName col-md form-control-lg" required="" autoFocus="" placeholder="First" onFocus={(event) => (event.target.setAttribute("placeholder", ""))}
+onBlur={(event) => (event.target.setAttribute("placeholder", "First"))}/>
                   <label htmlFor="inputFirstName">First</label>
                 </div>
               </div>
               <div className="col">
                 <div className="name row">
-                  <input type="text" id="inputLastName" className="col-md form-control-lg" required="" autoFocus="" />
+                  <input type="text" id="inputLastName" className="col-md form-control-lg" required="" autoFocus="" placeholder="Last" onFocus={(event) => (event.target.setAttribute("placeholder", ""))}
+onBlur={(event) => (event.target.setAttribute("placeholder", "Last"))}/>
                   <label htmlFor="inputLastName">Last</label>
                 </div>
               </div>
           </div>
 
           <div className="form-group row">
-            <input type="email" id="inputSignUpEmail" className="fullWidth form-control-lg" required="" autoFocus="" />
+            <input type="email" id="inputSignUpEmail" className="fullWidth form-control-lg" required="" autoFocus="" placeholder="Email" onFocus={(event) => (event.target.setAttribute("placeholder", ""))}
+onBlur={(event) => (event.target.setAttribute("placeholder", "Email")) }/>
             <label htmlFor="inputSignUpEmail">Email</label>
             </div>
           <div className="password form-group row">
-            <input type={this.state.isPasswordVisible ? "text" : "password"} id="inputSignUpPassword" className="fullWidth form-control-lg" required=""/>
+            <input type={this.state.isPasswordVisible ? "text" : "password"} id="inputSignUpPassword" className="fullWidth form-control-lg" required="" placeholder="Password" onFocus={(event) => (event.target.setAttribute("placeholder", ""))}
+onBlur={(event) => (event.target.setAttribute("placeholder", "Password"))}/>
             <label htmlFor="inputsignUpPassword">Password</label>
             <span className={this.state.isPasswordVisible ? "fas fa-eye-slash fa-lg" : "fas fa-eye fa-lg"} onClick={ this.toggleIcon }></span>
           </div>

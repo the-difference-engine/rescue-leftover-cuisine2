fbd5b0e379a45dd1725229ca2a0080e872ddc5a3
Meg Glavey <glaveym@gmail.com>
2019-03-04 19:13:03 -0600

    nearly completed form

--- frontend/src/components/CreateRecipeForm/CreateRecipeForm.js
+++ frontend/src/components/CreateRecipeForm/CreateRecipeForm.js
@@ -6,37 +6,84 @@ class CreateRecipeForm extends Component {
     return(
       <div className="createRecipeForm container-fluid">
         <div className="row">
-          <div className="col-md-12">
-            <div className="title">
-              <h1>Create Recipe</h1>
-            </div>
+          <div className="form-title">
+            <h1>Create Recipe</h1>
           </div>
         </div>
-        <div className="recipe-label">
+        <div className="row">
+        <div className="form-recipe-label">
           <div class="form-group">
             <label for="exampleFormControlTextarea1"><h3>Recipe Title</h3></label>
-            <textarea class="form-control" id="exampleFormControlTextarea1" rows="2"></textarea>
+            <input class="form-control" type="text" placeholder=""></input>
           </div> 
           <div class="form-group">
             <label for="exampleFormControlTextarea1"><h3>Recipe Description</h3></label>
-            <textarea class="form-control" id="exampleFormControlTextarea1" rows="4"></textarea>
+            <input class="form-control" type="text" placeholder=""></input>
           </div>
         </div>
-        <div className="recipe-form">
-          <div className="row">
-            <div className="dropdown">
-              <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown button</button>
-              <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                <a className="dropdown-item" href="">Action</a>
-                <a className="dropdown-item" href="">Another action</a>
-                <a className="dropdown-item" href="">Something else here</a>
-              </div>
+      </div>
+        <div className="row">
+          <div className="dropdown">
+            <h2>Difficulty</h2>
+            <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown button</button>
+            <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              <a className="dropdown-item" href="">Action</a>
+              <a className="dropdown-item" href="">Another action</a>
+              <a className="dropdown-item" href="">Something else here</a>
+            </div>
+          </div>
+          <div className="dropdown">
+            <h2>Duration</h2>
+            <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown button</button>
+            <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              <a className="dropdown-item" href="">Action</a>
+              <a className="dropdown-item" href="">Another action</a>
+              <a className="dropdown-item" href="">Something else here</a>
+            </div>
+          </div>
+          <div className="dropdown">
+            <h2>Servings</h2>
+            <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown button</button>
+            <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              <a className="dropdown-item" href="">Action</a>
+              <a className="dropdown-item" href="">Another action</a>
+              <a className="dropdown-item" href="">Something else here</a>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="form-tag-input">
+            <div class="form-group">
+              <label for="exampleFormControlTextarea1"><h3>Tags</h3></label>
+              <input class="form-control" type="text" placeholder=""></input>
+            </div> 
+          </div>
+        </div>
+        <div className="row">
+          <div className="form-ingredients">
+            <div class="form-group">
+              <label for="exampleFormControlTextarea1"><h3>Ingredients</h3></label>
+              <input class="form-control" type="text" placeholder=""></input>
+              <input class="form-control" type="text" placeholder=""></input>
+              <input class="form-control" type="text" placeholder=""></input>
+              <input class="form-control" type="text" placeholder=""></input>
+              <input class="form-control" type="text" placeholder=""></input>
+            </div>
+          </div>
+          <div className="form-directions">
+            <div class="form-group">
+              <label for="exampleFormControlTextarea1"><h3>Directions</h3></label>
+              <input class="form-control" type="text" placeholder=""></input>
+              <input class="form-control" type="text" placeholder=""></input>
+              <input class="form-control" type="text" placeholder=""></input>
+              <input class="form-control" type="text" placeholder=""></input>
+              <input class="form-control" type="text" placeholder=""></input>
             </div>
           </div>
         </div>
       </div>
-      )
-    }
+    )
+  }
 }
 
 export default CreateRecipeForm;

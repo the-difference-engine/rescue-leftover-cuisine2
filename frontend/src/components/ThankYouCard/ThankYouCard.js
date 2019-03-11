b19c76cd3d714a3fb5808af0d761a3761e5efef3
Katie <katmae83@gmail.com>
2019-03-10 21:54:31 -0500

    adding text

--- frontend/src/components/ThankYouCard/ThankYouCard.js
+++ frontend/src/components/ThankYouCard/ThankYouCard.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import '../../assets/checkmark-icon.png';
-import '../assets/grilled-chicken-dark.jpg';
+import Checkmark from '../../assets/checkmark-icon.png';
+import '../../assets/grilled-chicken-dark.jpg';
 import './ThankYouCard.css';
 
 
@@ -9,9 +9,9 @@ class ThankYouCard extends Component {
   render() { 
     return (
       <div className="loginCard">
-        
+        <img src={Checkmark} alt="checkmark icon" className="checkStyle"/>
         <h3 className="loginHeader">Thank you for<br/>Joining our Community!</h3>
-
+        <h6 className="welcomeText">Welcome to <br/> Rescuing Leftover Cuisine Recipes</h6>
           <div className="row">
             <button className="signUpButton btn btn-lg btn-block" type="submit">Continue to my Dashboard</button>
           </div>

4a6d475d70beca380d15dc41dd9b369e1c4d4815
Katie Larson <katmae83@gmail.com>
2019-03-11 16:05:38 -0500

    Feature/160/merge and fix login page (#168)
    * commenting out CSS for signin/sign up
    
    * creating background and cards
    
    * fixed corners and font
    
    * card layout corrections
    
    * change placeholder font size
    
    * card columns placement
    
    * change button color
    
    * aligned form fields with button
    
    * making fields in sign up full width
    
    * full width fields
    
    * css for password icon
    
    * format password and forgot password field
    
    * messing with card sizes and flex box
    
    * placeholder focus formatting
    
    * placeholder position and color change on focus
    
    * finish placeholder animation for all input fields
    
    * added new background image
    
    * realign first/last name fields
    
    * make width of cards 100%
    
    * code review corrections
    
    * styling onBlur and onFocus
    
    * fixed placeholder formatting
    
    * clean up css redundancies
    
    * Changed size of invisible label
    
    * deleting comments and unused code
    
    * delete file
    
    * adding constructor
    
    * change classname for input:focus
    
    * login header component created
    
    * footer spaced
    
    * added alt to img
    
    * change order of component importation
    
    * center login button and fields
    
    * narrowing cards
    
    * card margins changed to center cards with smaller gutter

--- frontend/src/components/SignIn/SignIn.js
+++ frontend/src/components/SignIn/SignIn.js
@@ -3,11 +3,12 @@ import './SignIn.css';
 
 
 class SignIn extends Component {
-
-  state = {
+  constructor(props) {
+    super(props)
+    this.state = {
     isPasswordVisible: false
-
   }
+}
 
   toggleIcon = () => {
     this.setState( prevState => ({
@@ -15,26 +16,30 @@ class SignIn extends Component {
     }));
   }
 
-
-  render() {
+  render() { 
     return (
-      <div>
-        <h3>Log In</h3>
-        <form className="form-signin">
-          <div className="form-label-group">
-            <input type="email" id="inputEmail4" className="form-control-lg" placeholder="Email address" required="" autoFocus="" />
-            <label htmlFor="inputEmail4">Email</label>
+      <div className="rightLoginCard loginCard">
+        <h3 className="loginHeader">Log In</h3>
+        <form className="form-signInUp">
+          <div className="form-group row">
+            <input type="email" id="inputSignInEmail" className=" sign-in-input fullWidth form-control-lg" required="" autoFocus="" placeholder="Email" onFocus={(event) => (event.target.setAttribute("placeholder", ""))}
+onBlur={(event) => (event.target.setAttribute("placeholder", "Email"))} />
+            <label htmlFor="inputSignInEmail">Email</label>
           </div>
 
-          <div className="form-label-group">
-            <input type={this.state.isPasswordVisible ? "text" : "password"} id="inputPassword3" className="form-control-lg" placeholder="Password" required="" />
-            <label htmlFor="inputPassword3">Password</label>
+          <div className="signInPassword form-group row">
+            <input type={this.state.isPasswordVisible ? "text" : "password"} id="inputSignInPassword" className="sign-in-input fullWidth form-control-lg" required="" placeholder="Password" onFocus={(event) => (event.target.setAttribute("placeholder", ""))}
+onBlur={(event) => (event.target.setAttribute("placeholder", "Password"))} />
+            <label htmlFor="inputsignUpPassword">Password</label>
             <span className={this.state.isPasswordVisible ? "fas fa-eye-slash fa-lg" : "fas fa-eye fa-lg"} onClick={ this.toggleIcon }></span>
           </div>
-          <div className="form-group">
+          <div className="forgotPassword form-group row">
             <a href="##########">I forgot my password</a>
           </div>
-          <button className="btn btn-lg btn-block" type="submit">Log In</button>
+          <div className="row">
+            <button className="signInButton signUpButton btn btn-lg btn-block" type="submit">Log In</button>
+          </div>
+
         </form>
       </div>
     );
--- frontend/src/components/SignUp/SignUp.js
+++ frontend/src/components/SignUp/SignUp.js
@@ -3,11 +3,12 @@ import './SignUp.css';
 
 
 class SignUp extends Component {
-
-  state = {
+  constructor(props) {
+    super(props)
+    this.state = {
     isPasswordVisible: false
-
   }
+}
 
   toggleIcon = () => {
     this.setState( prevState => ({
@@ -15,35 +16,42 @@ class SignUp extends Component {
     }));
   }
 
-
   render() {
     return (
-      <div>
-        <h3>Create an Account</h3>
-        <form className="form-signUp">
-          <div className="form-group">
-            <div className="form-label-group">
-              <input type="text" id="inputFirstName" className="form-control-lg" placeholder="First Name" required="" autoFocus="" />
-              <label htmlFor="inputFirstName">First Name</label>
-            </div>
-
-            <div className="form-label-group">
-              <input type="text" id="inputLastName" className="form-control-lg" placeholder="Last Name" required="" autoFocus="" />
-              <label htmlFor="inputLastName">Last Name</label>
-            </div>
+      <div className="leftLoginCard loginCard">
+        <h3 className="loginHeader">Create an Account</h3>
+        <form className="form-signInUp">
+          <div className="form-group row">
+              <div className="name col">
+                <div className="name row">
+                  <input type="text" id="inputFirstName" className="sign-in-input firstName col-md form-control-lg" required="" autoFocus="" placeholder="First" onFocus={(event) => (event.target.setAttribute("placeholder", ""))}
+onBlur={(event) => (event.target.setAttribute("placeholder", "First"))}/>
+                  <label htmlFor="inputFirstName">First</label>
+                </div>
+              </div>
+              <div className="col">
+                <div className="name row">
+                  <input type="text" id="inputLastName" className="sign-in-input col-md form-control-lg" required="" autoFocus="" placeholder="Last" onFocus={(event) => (event.target.setAttribute("placeholder", ""))}
+onBlur={(event) => (event.target.setAttribute("placeholder", "Last"))}/>
+                  <label htmlFor="inputLastName">Last</label>
+                </div>
+              </div>
           </div>
 
-          <div className="form-label-group">
-            <input type="email" id="inputEmail14" className="form-control-lg" placeholder="Email address" required="" autoFocus="" />
-            <label htmlFor="inputEmail14">Email</label>
-          </div>
-
-          <div className="form-label-group">
-            <input type={this.state.isPasswordVisible ? "text" : "password"} id="inputPassword4" className="form-control-lg" placeholder="Password" required="" />
-            <label htmlFor="inputPassword4">Password</label>
+          <div className="form-group row">
+            <input type="email" id="inputSignUpEmail" className="sign-in-input fullWidth form-control-lg" required="" autoFocus="" placeholder="Email" onFocus={(event) => (event.target.setAttribute("placeholder", ""))}
+onBlur={(event) => (event.target.setAttribute("placeholder", "Email")) }/>
+            <label htmlFor="inputSignUpEmail">Email</label>
+            </div>
+          <div className="password form-group row">
+            <input type={this.state.isPasswordVisible ? "text" : "password"} id="inputSignUpPassword" className="sign-in-input fullWidth form-control-lg" required="" placeholder="Password" onFocus={(event) => (event.target.setAttribute("placeholder", ""))}
+onBlur={(event) => (event.target.setAttribute("placeholder", "Password"))}/>
+            <label htmlFor="inputsignUpPassword">Password</label>
             <span className={this.state.isPasswordVisible ? "fas fa-eye-slash fa-lg" : "fas fa-eye fa-lg"} onClick={ this.toggleIcon }></span>
           </div>
-          <button className="btn btn-lg btn-block" type="submit">Sign Up</button>
+          <div className="form-group row">
+            <button className="signUpButton btn btn-lg btn-block" type="submit">Sign Up</button>
+          </div>
         </form>
       </div>
     );

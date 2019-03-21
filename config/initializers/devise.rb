a1d008431fac9aa7bd662725cd613757da126616
Katie <katmae83@gmail.com>
2019-03-20 19:15:01 -0500

    correcting spaces in comments

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -294,12 +294,6 @@ Devise.setup do |config|
 
   config.jwt do |jwt|
     jwt.secret = ENV['DEVISE_JWT_SECRET_KEY']
-    jwt.dispatch_requests = [
-      ['POST', %r{api/v1/auth/login}]
-    ]
-    jwt.revocation_requests = [
-      ['DELETE', %r{api/v1/auth/logout}]
-    ]
     jwt.expiration_time = ENV.fetch('DEVISE_JWT_EXPIRATION_TIME') { 1.day.to_i }
   end
 end

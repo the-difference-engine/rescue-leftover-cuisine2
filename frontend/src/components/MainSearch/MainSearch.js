faf316a0c7c85fce2787315350989deded363227
Jessica Smith <smith.j.m.13@gmail.com>
2019-03-25 20:25:25 -0500

    search button and error message added on diffrent branch with changes requested made because of adding eslint from staging

--- frontend/src/components/MainSearch/MainSearch.js
+++ frontend/src/components/MainSearch/MainSearch.js
@@ -6,6 +6,7 @@ class MainSearch extends Component {
     super(props);
     this.state = {
       searchTerm: '',
+      error: '',
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -16,24 +17,52 @@ class MainSearch extends Component {
     this.setState({ searchTerm: event.target.value });
   }
 
+  handleValidation() {
+    let error = '';
+    let isValid = true;
+    const { searchTerm } = this.state;
+    if (!searchTerm) {
+      error = 'Please enter at least one keyword to search';
+      isValid = false;
+    }
+    this.setState({ error });
+    return isValid;
+  }
+
   handleSubmit(event) {
     event.preventDefault();
     const { setSearchTerm } = this.props;
     const { searchTerm } = this.state;
-
-    setSearchTerm(searchTerm);
+    if (this.handleValidation()) {
+      setSearchTerm(searchTerm);
+    }
   }
 
   render() {
+    const { error } = this.state;
     return (
       <div className="mainSearch container-fluid">
         <div className="row">
           <div className="searchSection">
-            <p className="findText">Find Recipes from Rescuing Leftover Cuisine</p>
+            <p className="findText">
+              Find Recipes from Rescuing Leftover Cuisine
+            </p>
             <span className="search">
               <form className="search-form" onSubmit={this.handleSubmit}>
-                <input type="text" className="search-input" placeholder="Search by keywords" onChange={this.handleChange} />
+                <input
+                  type="text"
+                  className="search-input"
+                  placeholder="Search by keywords"
+                  onChange={this.handleChange}
+                />
+                <button type="submit" className="search-button">
+                  <img
+                    src="https://img.icons8.com/ios/30/000000/search.png"
+                    alt="search"
+                  />
+                </button>
               </form>
+              <div className="error-message">{error}</div>
             </span>
           </div>
         </div>

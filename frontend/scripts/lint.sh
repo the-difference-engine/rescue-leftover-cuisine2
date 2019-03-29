92c0e9f9980cf800722993f0b492a7a7f56d537d
Henry Firth <henrynf12@gmail.com>
2019-03-28 20:02:03 -0500

    Add CSS linting

---
+++ frontend/scripts/lint.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+dir="$(pwd)"
+
+echo "Linting JS... 🕵️"
+{
+  $dir/node_modules/.bin/eslint $1 src/App.js src/App.test.js src/index.js 'src/*/**/*.js' &&
+  echo "We're lint free! 🚀"
+  echo
+  sleep 1
+} || {
+  echo "Oh no! Lint! 😭"
+  exit 1
+}
+
+echo "Linting CSS... 🕵️"
+{
+  ./node_modules/stylelint/bin/stylelint.js $1 'src/**/*.css' &&
+  echo "We're lint free! 🚀"
+} || {
+   echo "Oh no! Lint! 😭"
+   exit 1
+}

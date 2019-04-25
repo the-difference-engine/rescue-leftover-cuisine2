c26ad9b46fa7ed877d104815c1628c18a6035e52
YOUR NAME <YOUR@EMAIL.com>
2019-04-25 18:59:12 -0500

    added link to outside donation page to the donate button

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { withRouter, Link } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 import rlcLogo from '../../assets/RLC_logo.png';
 import './Header.css';
 
@@ -20,9 +20,9 @@ const Header = ({ history }) => (
         Log In or Sign Up
       </button>
       <a href="https://www.rescuingleftovercuisine.org/sponsor-a-meal">
-      <button className="btn navbar-btn ml-4 px-4 btn-lg donateButton" type="button">
-      Donate
-      </button>
+        <button className="btn navbar-btn ml-4 px-4 btn-lg donateButton" type="button">
+        Donate
+        </button>
       </a>
     </div>
   </nav>

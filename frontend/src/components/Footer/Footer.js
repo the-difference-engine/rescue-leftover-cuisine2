18f266c84259c8572a93a9ec711490c56c75a6ef
Jessica Smith <smith.j.m.13@gmail.com>
2019-04-25 19:23:13 -0500

    deleted space in footer between by and icons8

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -29,7 +29,7 @@ const Footer = () => (
           <span className="designByCo">Fluid Designs</span>
           <p>
 App icons by
-            <a href="https://icons8.com/" target="_blank" rel="noopener noreferrer">icons8.</a>
+            <a href="https://icons8.com/" target="_blank" rel="noopener noreferrer"> icons8.</a>
           </p>
         </div>
 

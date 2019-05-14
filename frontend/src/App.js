139555e45bdba444c10f6217aeae046f7576eafd
Andrea Minhas <andrea.minhas@gmail.com>
2019-05-14 17:58:57 -0500

    background image and input fields

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 
-import SignUp from './components/SignUp/SignUp';
+import SignUp from "./components/SignUp/SignUp";
 
-import AdminPanel from './containers/AdminPanel/AdminPanel';
-import Homepage from './containers/Homepage/Homepage';
-import LoginPage from './containers/LoginPage/LoginPage';
-import CreateRecipe from './containers/CreateRecipe/CreateRecipe';
+import AdminPanel from "./containers/AdminPanel/AdminPanel";
+import Homepage from "./containers/Homepage/Homepage";
+import LoginPage from "./containers/LoginPage/LoginPage";
+import CreateRecipe from "./containers/CreateRecipe/CreateRecipe";
+import ResetPassword from "./containers/ResetPassword/ResetPasswordPage";
 
 const App = () => (
   <div>
@@ -16,6 +17,7 @@ const App = () => (
       <Route path="/signup" component={SignUp} />
       <Route path="/admin" component={AdminPanel} />
       <Route path="/recipe/new" component={CreateRecipe} />
+      <Route path="/resetpassword" component={ResetPassword} />
     </Switch>
   </div>
 );
---
+++ frontend/src/components/NewPassword/NewPassword.js
@@ -0,0 +1,104 @@
+/* global sessionStorage */
+/* eslint no-undef: "error" */
+
+import React, { Component } from "react";
+import { loginUser } from "../../lib/apiClient";
+import "./NewPassword.css";
+
+class SignIn extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isPasswordVisible: false,
+      email: "",
+      password: ""
+    };
+  }
+
+  toggleIcon = () => {
+    this.setState(prevState => ({
+      isPasswordVisible: !prevState.isPasswordVisible
+    }));
+  };
+
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  handleSubmit = (event, changeAuthorizedState) => {
+    event.preventDefault();
+    loginUser(this.state)
+      .then(response => {
+        sessionStorage.jwt = response.headers.authorization;
+        changeAuthorizedState(response.data.id);
+      })
+      .catch(error => {
+        if (error.message.includes("401")) {
+          const parentForm = document.getElementsByTagName("form")[1];
+          const errorDiv = document.createElement("p");
+          errorDiv.setAttribute("class", "errorMessage");
+          errorDiv.innerHTML = "Unable to log in. Check your email and password and try again.";
+          parentForm.insertAdjacentElement("beforeend", errorDiv);
+        }
+        console.log(error);
+      });
+  };
+
+  render() {
+    const { changeAuthorizedState } = this.props;
+    const { isPasswordVisible } = this.state;
+
+    return (
+      <div className="newPasswordCard newPasswordCard">
+        <h3 className="newPasswordHeader">Reset Password</h3>
+        <form className="form-newPassword" onSubmit={event => this.handleSubmit(event, changeAuthorizedState)}>
+          <div className="newPassword form-group row">
+            <input
+              type={isPasswordVisible ? "text" : "password"}
+              id="newPassword"
+              className="new-password-input fullWidth form-control-lg"
+              name="password"
+              required=""
+              placeholder="Password"
+              onFocus={event => event.target.setAttribute("placeholder", "")}
+              onBlur={event => event.target.setAttribute("placeholder", "Password")}
+              onChange={this.handleChange}
+              minLength="6"
+            />
+            <span
+              className={isPasswordVisible ? "fas fa-eye-slash fa-lg" : "fas fa-eye fa-lg"}
+              onClick={this.toggleIcon}
+            />
+          </div>
+
+          <div className="confrimPassword form-group row">
+            <input
+              type={isPasswordVisible ? "text" : "password"}
+              id="confirmPassword"
+              className="confirm-password-input fullWidth form-control-lg"
+              name="password"
+              required=""
+              placeholder="Confrim Password"
+              onFocus={event => event.target.setAttribute("placeholder", "")}
+              onBlur={event => event.target.setAttribute("placeholder", "Password")}
+              onChange={this.handleChange}
+              minLength="6"
+            />
+
+            <span
+              className={isPasswordVisible ? "fas fa-eye-slash fa-lg" : "fas fa-eye fa-lg"}
+              onClick={this.toggleIcon}
+            />
+          </div>
+          <div className="row">
+            <button className="resetPasswordButton btn btn-lg btn-block" type="submit" valid>
+              Reset Password
+            </button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SignIn;

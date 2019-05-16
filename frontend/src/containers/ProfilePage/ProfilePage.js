ea09d0cf0f6a7e4417fefe4069a78d45711a8a68
rmarro <30510953+rmarro@users.noreply.github.com>
2019-05-15 20:45:23 -0500

    recipe cards rendering in panel

--- frontend/src/containers/ProfilePage/ProfilePage.js
+++ frontend/src/containers/ProfilePage/ProfilePage.js
@@ -5,6 +5,7 @@ import {
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
 import profilePic from '../../assets/profilePic.PNG';
+import RecipeCard from '../../components/RecipeCard/RecipeCard';
 import './ProfilePage.css';
 
 class ProfilePage extends Component {
@@ -13,7 +14,65 @@ class ProfilePage extends Component {
 
     this.state = {
       activeTab: 'recipes',
-      // recipes: [],
+      recipes: [
+        {
+          id: 1,
+          title: 'Cauliflower Salad',
+          snippet: 'Soul-satisfying despite its healthy ingredients, this salad sings',
+          meal: 'DINNER',
+          ingredients: [
+            '1 head cauliflower',
+            '2 Tbsp olive oil',
+            '1 can oil packaged tuna',
+            '2 tsp Dijon mustard',
+            'Juice of 2 lemons',
+            'Salt and pepper',
+          ],
+          directions: [
+            'Roast the cauliflower',
+            'Make the dressing',
+            'Toss',
+          ],
+          difficulty: 'EASY',
+          duration: 60,
+          servings: 6,
+          photos: [
+            '/recipe-seeds/cauliflowersalad.jpg',
+          ],
+          created_at: '2019-05-02T00:21:41.113Z',
+          updated_at: '2019-05-02T00:21:41.113Z',
+          user_id: 1,
+        },
+        {
+          id: 3,
+          title: 'Deviled Eggs',
+          snippet: 'Everyone\'s favorite party snack!',
+          meal: 'SNACK',
+          ingredients: [
+            '12 eggs',
+            '2 Tbsp mustard',
+            '3 Tbsp mayonnaise',
+            '1 tsp paprika',
+            '6 cornichons, diced',
+            'Chives',
+          ],
+          directions: [
+            'Boil the eggs',
+            'Slice in half an remove yolks',
+            'Mix yolks with other ingredients except chives',
+            'Fill eggs with yolk mixture, garnish with paprika and chives',
+          ],
+          difficulty: 'MEDIUM',
+          duration: 30,
+          servings: 4,
+          photos: [
+            '/recipe-seeds/deviledeggs.jpg',
+          ],
+          created_at: '2019-05-02T00:21:41.142Z',
+          updated_at: '2019-05-02T00:21:41.142Z',
+          user_id: 1,
+        },
+      ],
     };
 
     this.toggle = this.toggle.bind(this);
@@ -30,7 +89,7 @@ class ProfilePage extends Component {
   }
 
   render() {
-    const { activeTab } = this.state;
+    const { activeTab, recipes } = this.state;
 
     return (
       <div className="profile-page container-fluid">
@@ -118,14 +177,20 @@ class ProfilePage extends Component {
               </NavItem>
             </Nav>
           </div>
-          <div className="col-md-9 user-pane">
+          <div className="col-md-9 right-pane">
             <TabContent activeTab={activeTab}>
               <TabPane tabId="recipes">
-                  Recipes will go here
-                {/* <Recipes /> */}
+                <div id="cards-wrapper">
+                  {recipes.map(recipe => (
+                    <RecipeCard
+                      {...recipe}
+                      key={recipe.id}
+                    />
+                  ))}
+                </div>
               </TabPane>
               <TabPane tabId="settings">
-                  Settings will go here
+                Settings will go here
               </TabPane>
             </TabContent>
           </div>

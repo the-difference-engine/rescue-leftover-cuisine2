e1c1828ee79c900828866617ba6b0819d9d7609e
ashzagorski <ash.zagorski@gmail.com>
2019-05-23 20:37:03 -0500

    line spacing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-
 import SignUp from './components/SignUp/SignUp';
-
 import AdminPanel from './containers/AdminPanel/AdminPanel';
 import Homepage from './containers/Homepage/Homepage';
 import LoginPage from './containers/LoginPage/LoginPage';
@@ -13,16 +11,12 @@ import Recipe from './containers/RecipePage/Recipe';
 const App = () => (
   <div>
     <Switch>
-
-
       <Route exact path="/" component={Homepage} />
       <Route path="/login" component={LoginPage} />
       <Route path="/signup" component={SignUp} />
       <Route path="/admin" component={AdminPanel} />
       <Route path="/recipe/new" component={CreateRecipe} />
       <Route path="/recipe" component={Recipe} />
-
-
     </Switch>
 
   </div>

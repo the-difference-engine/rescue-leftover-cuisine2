0213dcc021c85df3bf24658ab998421d7f6887af
Nicholas Harris <nharris614@gmail.com>
2019-06-10 19:51:55 -0500

    Added CSS for <a> to limit linking to the logo

--- frontend/src/components/LogInPageHeader/LogInPageHeader.js
+++ frontend/src/components/LogInPageHeader/LogInPageHeader.js
@@ -4,9 +4,11 @@ import './LogInPageHeader.css';
 
 const LogInPageHeader = () => (
   <div className="container-fluid">
+    
     <a href="/">
       <img src={Logo} className="logoCenter" alt="RLC Logo" />
     </a>
+      
   </div>
 );
 

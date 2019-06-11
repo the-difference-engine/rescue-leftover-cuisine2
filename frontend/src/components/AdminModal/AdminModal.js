c07ea17f2432f96e7beb7c2ca854d7d962def26c
rmarro <30510953+rmarro@users.noreply.github.com>
2019-06-10 19:34:22 -0500

    fixed style of edit user modal

--- frontend/src/components/AdminModal/AdminModal.js
+++ frontend/src/components/AdminModal/AdminModal.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import {
-  Button, Modal, ModalHeader, ModalBody, ModalFooter,
+  Button, Modal, ModalHeader, ModalBody, ModalFooter, Form, FormGroup, Label, Col, Input,
 } from 'reactstrap';
+import './AdminModal.css';
 
 class AdminModal extends React.Component {
   constructor(props) {
@@ -12,29 +13,39 @@ class AdminModal extends React.Component {
 
   render() {
     const {
-      modal, toggle, selectedUser, className,
+      modal, toggle, selectedUser,
     } = this.props;
+
+    const closeBtn = <button type="button" className="edit-user-close-button" onClick={toggle}><img src="https://img.icons8.com/windows/32/9b9b9b/cancel.png" alt="close" /></button>;
+
     return (
       <div>
-        <Modal isOpen={modal} toggle={toggle} className={className}>
-          <ModalHeader toggle={toggle} charCode="X">Edit User</ModalHeader>
+        <Modal className="edit-user-modal" isOpen={modal} toggle={toggle} backdrop={false}>
+          <ModalHeader toggle={toggle} close={closeBtn}>Edit User Information</ModalHeader>
           <ModalBody>
-            <p>
-              First Name:
-              {selectedUser.first_name}
-            </p>
-            <p>
-              Last Name:
-              {selectedUser.last_name}
-            </p>
-            <p>
-              Email:
-              {selectedUser.email}
-            </p>
+            <Form>
+              <FormGroup row>
+                <Label for="firstName" sm={4}>First Name</Label>
+                <Col sm={8}>
+                  <Input type="text" name="text" id="firstName" defaultValue={selectedUser.first_name} />
+                </Col>
+              </FormGroup>
+              <FormGroup row>
+                <Label for="lastName" sm={4}>Last Name</Label>
+                <Col sm={8}>
+                  <Input type="text" name="text" id="lastName" defaultValue={selectedUser.last_name} />
+                </Col>
+              </FormGroup>
+              <FormGroup row>
+                <Label for="email" sm={4}>Email</Label>
+                <Col sm={8}>
+                  <Input type="email" name="email" id="email" defaultValue={selectedUser.email} />
+                </Col>
+              </FormGroup>
+            </Form>
           </ModalBody>
           <ModalFooter>
-            <Button color="primary" onClick={toggle}>Do Something</Button>
-            <Button color="secondary" onClick={toggle}>Cancel</Button>
+            <Button className="edit-user-save-button" onClick={toggle}>Save</Button>
           </ModalFooter>
         </Modal>
       </div>

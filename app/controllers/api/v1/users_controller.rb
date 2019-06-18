31c4d179ea742715535a7d1dbe93c60d22a91fbe
rmarro <30510953+rmarro@users.noreply.github.com>
2019-06-17 20:13:57 -0500

    fixed button layout and cleaned up some comments

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -13,14 +13,13 @@ class Api::V1::UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      flash[:notice] = "Successfully updated user."
       redirect_to admin_path
     else
       render 'edit'
     end
   end
 
-  # change this maybe (this helps format the data to spit it back into update above)
+  # fix this for above (this helps format the data to spit it back into update above)
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email)
   end

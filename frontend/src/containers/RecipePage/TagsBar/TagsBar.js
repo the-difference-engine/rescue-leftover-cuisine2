bce71d95eb0c9415d2ef6687d84b5bc0458fdb77
ashzagorski <ash.zagorski@gmail.com>
2019-06-24 18:34:10 -0500

    spacing

--- frontend/src/containers/RecipePage/TagsBar/TagsBar.js
+++ frontend/src/containers/RecipePage/TagsBar/TagsBar.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
-
 import AddTag from './AddTag/AddTag';
 import SingleTag from './SingleTag/SingleTag';
-
 import './TagsBar.css';
 
 export default class TagsBar extends Component {

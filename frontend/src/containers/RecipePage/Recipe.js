d1715f2f82cf017be62443711438ebd71f6b44ed
ashzagorski <ash.zagorski@gmail.com>
2019-06-24 20:33:44 -0500

    linting closer

--- frontend/src/containers/RecipePage/Recipe.js
+++ frontend/src/containers/RecipePage/Recipe.js
@@ -113,16 +113,16 @@ class Recipe extends Component {
         <div id="direction-container" className="col-sm-10 offset-sm-1">
           <h1 id="direction-title">Directions</h1>
           <ul className="direction-list">
-                {directions.map((direction, index) => (
+              {directions.map((direction, index) => (
               <div className="single-direction">
-                      <span className="direction-index">{index + 1}</span>
-                      <p className="direction-action">{direction}</p>
-                      <br />
+                <span className="direction-index">{index + 1}</span>
+                <p className="direction-action">{direction}</p>
+                <br />
               </div>
                 ))
             }
-             </ul>
-          </div>
+          </ul>
+        </div>
         <div className="row">
           <Footer />
         </div>

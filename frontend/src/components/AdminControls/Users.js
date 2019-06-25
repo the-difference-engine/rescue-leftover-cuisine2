29d9ffbed0caa1adb7dd3392f6799d79d515f75c
rmarro <30510953+rmarro@users.noreply.github.com>
2019-06-24 20:21:03 -0500

    changed Users.js back to functional component with hooks

--- frontend/src/components/AdminControls/Users.js
+++ frontend/src/components/AdminControls/Users.js
@@ -1,173 +1,157 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import BootstrapTable from 'react-bootstrap-table-next';
 import paginationFactory from 'react-bootstrap-table2-paginator';
 import { Type } from 'react-bootstrap-table2-editor';
 import './AdminTables.css';
 import AdminEditModal from '../AdminEditModal/AdminEditModal';
 
-class Users extends Component {
-  constructor(props) {
-    super(props);
+const Users = ({ users, refreshUsers }) => {
+  const [editModal, setEditModal] = useState(false);
+  const [selectedUser, setSelectedUser] = useState({});
 
-    this.state = {
-      editModal: false,
-      selectedUser: {},
-    };
-    this.toggleEditModal = this.toggleEditModal.bind(this);
-  }
+  const toggleEditModal = () => {
+    setEditModal(!editModal);
+  };
 
-  toggleEditModal() {
-    this.setState(prevState => ({
-      editModal: !prevState.editModal,
-    }));
-  }
+  const columns = [
+    {
+      dataField: 'inStock', // name?
+      text: '',
+      headerStyle: {
+        borderTop: 'none',
+        borderBottom: 'none',
+      },
+      style: {
+        paddingLeft: '10px',
+      },
+      formatter: (_cellContent, row) => (
+        <div className="checkbox">
+          <label htmlFor="adminCheckbox" className="admin-checkbox-container user-checkbox-spacing">
+            <input type="checkbox" id="adminCheckbox" checked={row.inStock} />
+            <span className="admin-table-checkmark" />
+          </label>
+        </div>
+      ),
+    },
+    {
+      dataField: 'full_name',
+      text: 'User Name',
+      style: { textAlign: 'center' },
+      headerStyle: {
+        textAlign: 'center',
+        borderTop: 'none',
+        borderBottom: 'none',
+      },
+    },
+    {
+      dataField: 'created_at',
+      text: 'Member Since',
+      style: { textAlign: 'center' },
+      headerStyle: {
+        textAlign: 'center',
+        borderTop: 'none',
+        borderBottom: 'none',
+      },
+      formatter: (cell) => {
+        let dateObj = cell;
+        if (typeof cell !== 'object') {
+          dateObj = new Date(cell);
+        }
 
-  render() {
-    const { users, refreshUsers } = this.props;
-    const columns = [
+        const day = `0${dateObj.getUTCDate()}`.slice(-2);
+        const month = `0${dateObj.getUTCMonth() + 1}`;
+        const year = dateObj.getUTCFullYear();
+        return `${month}/${day}/${year}`;
+      },
+      editor: {
+        type: Type.DATE,
+      },
+    },
+    {
+      dataField: 'recipes',
+      text: 'Number Of Recipes',
+      align: 'center',
+      style: {
+        paddingRight: '80px',
+      },
+      headerStyle: {
+        paddingRight: '80px',
+        textAlign: 'center',
+        borderTop: 'none',
+        borderBottom: 'none',
+      },
+      formatter: cell => `${cell.length}`,
+    },
+    {
+      dataField: 'edit',
+      text: '',
+      headerStyle: {
+        textAlign: 'center',
+        borderTop: 'none',
+        borderBottom: 'none',
+      },
+      align: 'left',
+      formatter: () => (
+        <div>
+          <button type="button" className="admin-edit-button" onClick={toggleEditModal}>
+            <img
+              src="https://img.icons8.com/windows/32/000000/edit.png"
+              alt="edit"
+            />
+          </button>
+          <button type="button" className="admin-edit-button">
+            <img
+              src="https://img.icons8.com/windows/32/000000/cancel.png"
+              alt="delete"
+            />
+          </button>
+        </div>
+      ),
+    },
+  ];
+
+  const options = {
+    hideSizePerPage: true,
+    sizePerPageList: [
       {
-        dataField: 'inStock', // name?
-        text: '',
-        headerStyle: {
-          borderTop: 'none',
-          borderBottom: 'none',
-        },
-        style: {
-          paddingLeft: '10px',
-        },
-        formatter: (_cellContent, row) => (
-          <div className="checkbox">
-            <label htmlFor="adminCheckbox" className="admin-checkbox-container user-checkbox-spacing">
-              <input type="checkbox" id="adminCheckbox" checked={row.inStock} />
-              <span className="admin-table-checkmark" />
-            </label>
-          </div>
-        ),
+        text: '5',
+        value: 20,
       },
       {
-        dataField: 'full_name',
-        text: 'User Name',
-        style: { textAlign: 'center' },
-        headerStyle: {
-          textAlign: 'center',
-          borderTop: 'none',
-          borderBottom: 'none',
-        },
+        text: 'All',
+        value: users.length,
       },
-      {
-        dataField: 'created_at',
-        text: 'Member Since',
-        style: { textAlign: 'center' },
-        headerStyle: {
-          textAlign: 'center',
-          borderTop: 'none',
-          borderBottom: 'none',
-        },
-        formatter: (cell) => {
-          let dateObj = cell;
-          if (typeof cell !== 'object') {
-            dateObj = new Date(cell);
-          }
+    ],
+  };
 
-          const day = `0${dateObj.getUTCDate()}`.slice(-2);
-          const month = `0${dateObj.getUTCMonth() + 1}`;
-          const year = dateObj.getUTCFullYear();
-          return `${month}/${day}/${year}`;
-        },
-        editor: {
-          type: Type.DATE,
-        },
-      },
-      {
-        dataField: 'recipes',
-        text: 'Number Of Recipes',
-        align: 'center',
-        style: {
-          paddingRight: '80px',
-        },
-        headerStyle: {
-          paddingRight: '80px',
-          textAlign: 'center',
-          borderTop: 'none',
-          borderBottom: 'none',
-        },
-        formatter: cell => `${cell.length}`,
-      },
-      {
-        dataField: 'edit',
-        text: '',
-        headerStyle: {
-          textAlign: 'center',
-          borderTop: 'none',
-          borderBottom: 'none',
-        },
-        align: 'left',
-        formatter: () => (
-          <div>
-            <button type="button" className="admin-edit-button" onClick={this.toggleEditModal}>
-              <img
-                src="https://img.icons8.com/windows/32/000000/edit.png"
-                alt="edit"
-              />
-            </button>
-            <button type="button" className="admin-edit-button">
-              <img
-                src="https://img.icons8.com/windows/32/000000/cancel.png"
-                alt="delete"
-              />
-            </button>
-          </div>
-        ),
-      },
-    ];
+  // This is a react-bootstrap-table2 way of
+  // getting the row index, which we use to get
+  // the corresponding user data
+  const rowEvents = {
+    onClick: (e, row, rowIndex) => {
+      setSelectedUser(users[rowIndex]);
+    },
+  };
 
-    const options = {
-      hideSizePerPage: true,
-      sizePerPageList: [
-        {
-          text: '5',
-          value: 20,
-        },
-        {
-          text: 'All',
-          value: users.length,
-        },
-      ],
-    };
-
-    // This is a react-bootstrap-table2 way of
-    // getting the row index, which we use to get
-    // the corresponding user data
-    const rowEvents = {
-      onClick: (e, row, rowIndex) => {
-        this.setState({
-          selectedUser: users[rowIndex],
-        });
-      },
-    };
-
-    const { editModal, selectedUser } = this.state;
-
-    return (
-      <Fragment>
-        <h1 className="admin-users-title">Admin Dashboard</h1>
-        <BootstrapTable
-          keyField="id"
-          data={users}
-          columns={columns}
-          rowEvents={rowEvents}
-          bordered={false}
-          pagination={paginationFactory(options)}
-        />
-        <AdminEditModal
-          editModal={editModal}
-          toggleEditModal={this.toggleEditModal}
-          selectedUser={selectedUser}
-          refreshUsers={refreshUsers}
-        />
-      </Fragment>
-    );
-  }
-}
+  return (
+    <Fragment>
+      <h1 className="admin-users-title">Admin Dashboard</h1>
+      <BootstrapTable
+        keyField="id"
+        data={users}
+        columns={columns}
+        rowEvents={rowEvents}
+        bordered={false}
+        pagination={paginationFactory(options)}
+      />
+      <AdminEditModal
+        editModal={editModal}
+        toggleEditModal={toggleEditModal}
+        selectedUser={selectedUser}
+        refreshUsers={refreshUsers}
+      />
+    </Fragment>
+  );
+};
 
 export default Users;

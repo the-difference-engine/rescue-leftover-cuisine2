ffa09f85ac3511925381dafa61f0466a603fcc4e
Nicholas Harris <nharris614@gmail.com>
2019-07-31 16:05:34 -0500

    Changed component from class to functional

--- frontend/src/components/AdminSuspendModal/AdminSuspendModal.js
+++ frontend/src/components/AdminSuspendModal/AdminSuspendModal.js
@@ -5,57 +5,49 @@ import {
 import './AdminSuspendModal.css';
 import { suspendUser } from '../../lib/apiClient';
 
-class AdminSuspendModal extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
-  }
+function AdminSuspendModal(props) {
+  const {
+    toggleSuspendModal, suspendModal, selectedUser, refreshUsers,
+  } = props;
 
-  render() {
-    const {
-      toggleSuspendModal, suspendModal, selectedUser, refreshUsers,
-    } = this.props;
+  const handleSuspend = () => {
+    suspendUser(selectedUser.id, !selectedUser.is_suspended).then(() => {
+      toggleSuspendModal();
+      refreshUsers();
+    });
+  };
 
-    const handleSuspend = () => {
-      suspendUser(selectedUser.id, !selectedUser.is_suspended).then(() => {
-        toggleSuspendModal();
-        refreshUsers();
-      });
-    };
+  const closeBtn = (
+    <button
+      type="button"
+      className="suspend-user-close-button"
+      onClick={toggleSuspendModal}
+    >
+      <img src="https://img.icons8.com/windows/32/9b9b9b/cancel.png" alt="close" />
+    </button>
+  );
 
-    const closeBtn = (
-      <button
-        type="button"
-        className="suspend-user-close-button"
-        onClick={toggleSuspendModal}
-      >
-        <img src="https://img.icons8.com/windows/32/9b9b9b/cancel.png" alt="close" />
-      </button>
-    );
+  return (
+    <div>
+      <Modal className="suspend-user-modal" isOpen={suspendModal} toggleSuspendModal={toggleSuspendModal} backdrop={false}>
+        <ModalHeader toggleSuspendModal={toggleSuspendModal} close={closeBtn}>
+          Suspend User?
+        </ModalHeader>
+        <ModalBody>
+          Clicking &quot;Suspended&quot; will prevent the user from logging into the app.
 
-    return (
-      <div>
-        <Modal className="suspend-user-modal" isOpen={suspendModal} toggleSuspendModal={toggleSuspendModal} backdrop={false}>
-          <ModalHeader toggleSuspendModal={toggleSuspendModal} close={closeBtn}>
-            Suspend User?
-          </ModalHeader>
-          <ModalBody>
-            Clicking &quot;Suspended&quot; will prevent the user from logging into the app.
+            The user will remain suspended until this status is removed by an administrator.
 
-              The user will remain suspended until this status is removed by an administrator.
-
-              If you do not wish to suspend the user, close this dialog box.
-          </ModalBody>
-          <ModalFooter>
-            <Button className="suspend-user-save-button" color="primary" onClick={handleSuspend}>
-              Suspend
-            </Button>
-          </ModalFooter>
-        </Modal>
-      </div>
-    );
-  }
+            If you do not wish to suspend the user, close this dialog box.
+        </ModalBody>
+        <ModalFooter>
+          <Button className="suspend-user-save-button" color="primary" onClick={handleSuspend}>
+            Suspend
+          </Button>
+        </ModalFooter>
+      </Modal>
+    </div>
+  );
 }
 
 export default AdminSuspendModal;

79aa5f567fa6018e3964e8fd5421f9601cb493b0
Andrea Minhas <andrea.minhas@gmail.com>
2019-08-07 20:48:26 -0500

    remove console log

--- frontend/src/lib/apiClient.js
+++ frontend/src/lib/apiClient.js
@@ -7,62 +7,65 @@ const baseURL = process.env.REACT_APP_API_URL || 'http://localhost:3000';
 
 const apiClient = axios.create({
   baseURL,
-  headers: { 'Content-Type': 'application/json' },
+  headers: { 'Content-Type': 'application/json' }
 });
 
 // AUTHENTICATION
 
-const createUser = data => apiClient.post('api/v1/auth', {
-  user: {
-    first_name: data.firstName,
-    last_name: data.lastName,
-    email: data.email,
-    password: data.password,
-  },
-});
+const createUser = data =>
+  apiClient.post('api/v1/auth', {
+    user: {
+      first_name: data.firstName,
+      last_name: data.lastName,
+      email: data.email,
+      password: data.password
+    }
+  });
 
-const loginUser = (data) => {
+const loginUser = data => {
   const credentials = { email: data.email, password: data.password };
   return apiClient.post('api/v1/auth/login', {
-    user: credentials,
+    user: credentials
   });
 };
 
 // CURRENT USER
 
-const getCurrentUser = () => apiClient.get('api/v1/auth', {
-  headers: {
-    Authorization: sessionStorage.jwt,
-  },
-});
+const getCurrentUser = () =>
+  apiClient.get('api/v1/auth', {
+    headers: {
+      Authorization: sessionStorage.jwt
+    }
+  });
 
 // USERS
 
-const getUsers = () => apiClient.get('api/v1/users')
-  .then(results => results.data);
+const getUsers = () => apiClient.get('api/v1/users').then(results => results.data);
 
-const getUser = userId => apiClient.get(`api/v1/users/${userId}`, {
-  headers: {
-    Authorization: sessionStorage.jwt,
-  },
-});
+const getUser = userId =>
+  apiClient.get(`api/v1/users/${userId}`, {
+    headers: {
+      Authorization: sessionStorage.jwt
+    }
+  });
 
-const suspendUser = (userId, isSuspended) => apiClient.patch(`api/v1/users/${userId}`, {
-  is_suspended: isSuspended,
-});
+const suspendUser = (userId, isSuspended) =>
+  apiClient.patch(`api/v1/users/${userId}`, {
+    is_suspended: isSuspended
+  });
 
-const adminEditUser = (data, userId) => apiClient.patch(`api/v1/users/${userId}`, {
-  first_name: data.firstName.value,
-  last_name: data.lastName.value,
-  email: data.email.value,
-});
+const adminEditUser = (data, userId) =>
+  apiClient.patch(`api/v1/users/${userId}`, {
+    first_name: data.firstName.value,
+    last_name: data.lastName.value,
+    email: data.email.value
+  });
 
 // RECIPES
 
-const getRecipes = (search) => {
+const getRecipes = search => {
   const query = search ? `?search=${search}` : '';
-  return apiClient.get(`api/v1/recipes${query}`)
-    .then(results => results.data);
+  return apiClient.get(`api/v1/recipes${query}`).then(results => results.data);
 };
 
 const getRecipe = id => apiClient.get(`api/v1/recipes/${id}`);
@@ -70,23 +73,32 @@ const getRecipe = id => apiClient.get(`api/v1/recipes/${id}`);
 // PASSWORD REQUEST
 
 const resetPassword = (password, resetPasswordToken) => {
-  console.log(resetPasswordToken);
   return apiClient.put('/api/v1/password', {
     user: {
       password,
-      reset_password_token: resetPasswordToken,
-    },
+      reset_password_token: resetPasswordToken
+    }
   });
 };
 
-const requestPasswordReset = email => apiClient.post('/api/v1/password', {
-  user: {
-    email,
-  },
-});
+const requestPasswordReset = email =>
+  apiClient.post('/api/v1/password', {
+    user: {
+      email
+    }
+  });
 
 export default apiClient;
 export {
-  createUser, loginUser, getCurrentUser, getUsers, getUser, getRecipes, getRecipe, adminEditUser,
-  resetPassword, requestPasswordReset, suspendUser,
+  createUser,
+  loginUser,
+  getCurrentUser,
+  getUsers,
+  getUser,
+  getRecipes,
+  getRecipe,
+  adminEditUser,
+  resetPassword,
+  requestPasswordReset,
+  suspendUser
 };

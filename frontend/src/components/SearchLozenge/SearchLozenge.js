1071e005e07726786edef50f27cf00c360c26e1e
Samidh Desai <34141113+SamDesai333@users.noreply.github.com>
2019-08-08 20:37:27 -0500

    Feature/235/edit search (#298)
    * adds new lozenge component
    
    * adds lozenge bubbles with search terms to homepage
    
    * adds recipe search result number
    
    * has lozenges and search result number appear after a search has been made
    
    *  adds some styling to results counter and fixes some changes that were requested
    
    * fixes some css on the lozenge bubbles
    
    * fixes changes requested
    
    * fixes indentation errors
    
    * adds lozenge delete functionality and embedds both components into RecipeSearchList
    
    * adds semicolon in ResultsCounter
    
    * Remove parsed from props

---
+++ frontend/src/components/SearchLozenge/SearchLozenge.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import './SearchLozenge.css';
+
+const SearchLozenge = (props) => {
+  const { searchTerm } = props;
+
+  const capitalize = lozenge => lozenge.charAt(0).toUpperCase() + lozenge.slice(1).toLowerCase();
+
+  const lozenges = searchTerm.split(' ');
+
+  function handleClick(event) {
+    const targetLozenge = event.currentTarget.textContent;
+    const lozengeValue = targetLozenge.substring(0, targetLozenge.length - 1).toLowerCase();
+    const newSearchTerm = searchTerm.replace(lozengeValue, '').trim();
+    const { history } = props;
+    const queryString = newSearchTerm ? `/?q=${newSearchTerm}` : '';
+    history.push(queryString);
+  }
+
+  return (
+    <div className="searchLozenge container">
+      <div className="row">
+        <div className="lozengeBubbles">
+          {lozenges.map(lozenge => (
+            <div onClick={handleClick} className="lozenge">
+              {capitalize(lozenge)}
+              <button className="closeLozenge" type="button">x</button>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+SearchLozenge.propTypes = {
+  searchTerm: PropTypes.string,
+};
+
+SearchLozenge.defaultProps = {
+  searchTerm: '',
+};
+
+export default withRouter(SearchLozenge);

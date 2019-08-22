cb55014495c8644787020622660c5a368aa303af
vishnu <vvichugit@gmail.com>
2019-08-21 21:25:42 -0500

    added the route on the backend and creted confirmations controller

--- frontend/src/components/RedirectToHome/Redirectss.js
+++ frontend/src/components/RedirectToHome/Redirectss.js
@@ -3,31 +3,23 @@ import { withRouter } from 'react-router-dom';
 import axios from 'axios';
 
 class Redirectss extends Component {
-
-
   componentWillMount() {
     const url = (window.location.href);
     const ind = url.split('/');
     const l = ind.length;
 
     const c = (ind[l - 1]);
-   
-    //To get the user - data: 
+    // To get the user - data:
     axios.get(`http://localhost:3000/api/v1/${c}`).then((response) => {
-      
-          var params = {
-            user_id: response.data.id,
-            email: response.data.email
-          }
-   
-      axios.put("http://localhost:3000/api/v1/confirms", params).then((res) => {
-        console.log("The data from the backend: ");
-         console.log(res.data);
-      })    
-
+      const params = {
+        user_id: response.data.id,
+        email: response.data.email,
+      };
+      axios.put('http://localhost:3000/api/v1/confirms', params).then((res) => {
+        console.log('The data from the backend: ');
+        console.log(res.data);
+      });
     });
-
-
   }
 
   render() {

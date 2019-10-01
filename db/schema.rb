f98bcffd4b7c53111d9acc2303aa768bab9a2e29
Bryan Vanderhoof <github@brynomial.com>
2019-09-30 20:17:37 -0500

    Fix logout

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_11_003854) do
+ActiveRecord::Schema.define(version: 2019_10_01_005214) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "jwt_blacklist", force: :cascade do |t|
     t.string "jti", null: false
+    t.datetime "exp", null: false
     t.index ["jti"], name: "index_jwt_blacklist_on_jti"
   end
 

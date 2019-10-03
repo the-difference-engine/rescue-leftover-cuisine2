ba53f01364d79321f6765b8568fb3c7d720b7cbe
Andrea Minhas <andrea.minhas@gmail.com>
2019-10-02 20:06:08 -0500

    added proptypes

--- frontend/src/components/RecipeComments/RecipeComments.js
+++ frontend/src/components/RecipeComments/RecipeComments.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import { createComment } from '../../lib/apiClient';
-// import PropTypes from 'prop-types';
 import './RecipeComments.css';
 
 const RecipeComments = ({ comments, recipeId }) => {
@@ -15,8 +15,7 @@ const RecipeComments = ({ comments, recipeId }) => {
   };
 
   const renderComments = () => {
-    // console.log(comments);
-    // comments.map(comment => console.log(comment));
+    console.log(comments[0]);
   };
 
   return (
@@ -35,12 +34,47 @@ const RecipeComments = ({ comments, recipeId }) => {
   );
 };
 
-// RecipeComments.defaultProps = {
-//   comments: [],
-// };
 
-// RecipeComments.PropTypes = {
-//   comments: PropTypes.arrayOf(Proptypes.shape({})),
-// }
+RecipeComments.propTypes = {
+  comments: PropTypes.arrayOf(PropTypes.shape({
+    body: PropTypes.string,
+    created_at: PropTypes.string,
+    id: PropTypes.number,
+    recipe_id: PropTypes.id,
+    updated_at: PropTypes.string,
+    user: PropTypes.shape({
+      created_at: PropTypes.string,
+      email: PropTypes.string,
+      first_name: PropTypes.string,
+      id: PropTypes.number,
+      interests: PropTypes.number,
+      is_admin: PropTypes.bool,
+      is_suspended: PropTypes.bool,
+      last_name: PropTypes.string,
+      profile_photo: PropTypes.string,
+      updated_at: PropTypes.string,
+      username: PropTypes.string,
+    }),
+    user_id: PropTypes.number,
+  })),
+};
+
+RecipeComments.defaultProps = {
+  comments: [{
+    body: "comment",
+    created_at: "2019-10-03T00:31:25.357Z",
+    id: 10,
+    recipe_id: 28,
+    updated_at: "2019-10-03T00:31:25.357Z",
+    user: {
+      id: 1,
+      email: 'bob@domain.com',
+      created_at: "2019-09-24T01:03:56.610Z",
+      updated_at: "2019-09-24T01:03:56.610Z",
+      username: "Bob@RLC",
+    },
+    user_id: 1,
+  }],
+};
 
 export default RecipeComments;

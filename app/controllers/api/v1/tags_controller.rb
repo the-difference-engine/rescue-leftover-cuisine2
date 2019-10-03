438c553f23a37db886f6622318d5ea5a393c226f
Rimma <rim23ma@gmail.com>
2019-10-02 21:07:47 -0500

    seed data with tags lunch and dinner and seeded assosiations beetween tags and recipe1

--- app/controllers/api/v1/tags_controller.rb
+++ app/controllers/api/v1/tags_controller.rb
@@ -2,16 +2,16 @@ class TagController < ApplicationController
 
     def create
         @tag = Tag.new(tag_params)
-        #@tag = Tag.find_or_create_by(title: tags_params[:title])
+        @tag = Tag.find_or_create_by(title: tags_params[:title])
         #params[:recipe_id]
-        
+        #@recipes = @tag.recipes
         @tag.save
         render json: @tag
     end
- 
-    
+
+
     def tag_params
         params.require(:tag).permit(:title)
     end
-    
+
     end

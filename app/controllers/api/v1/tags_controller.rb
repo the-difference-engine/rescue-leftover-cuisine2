cb378a9c04487922d180b25c8787ad7f5d6a98d7
Nicoleta <nicoleta.platniuc@gmail.com>
2019-10-03 22:28:47 -0500

    add search functionality for tags

--- app/controllers/api/v1/tags_controller.rb
+++ app/controllers/api/v1/tags_controller.rb
@@ -1,23 +1,18 @@
 class TagController < ApplicationController
 
-    def create
-        @tag = Tag.new(tag_params)
+    def index
+        if params[:search]
+            @tags = Tag.search(params[:search])
+        else
+          @tags = Tag.all
+        end
+          render json: @tag
+      end
 
-        @tag.recipes << Tag.find_or_create_by(title: tag_params[:title])
-        
-        @tag.recipes.where(title: tag_params[:title]).first_or_create
-        # @tag = Tag.find_or_create_by(title: tag_params[:title])
-        # params[:recipe_id]
 
-        @tag.recipes.build({title: tag_params[:title]})
-
-        @tag.save
-        render json: @tag
-    end
- 
     
-    def tag_params
-        params.require(:tag).permit(:title)
-    end
+    # def tag_params
+    #     params.require(:tag).permit(:title)
+    # end
     
     end

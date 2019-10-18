77edff67d6e423490860c95e1fb892736b66a8c4
Rimma <rim23ma@gmail.com>
2019-10-17 22:31:43 -0500

    option values for adding tags in recipe are outside of the reactselect container and added button to delete tags

--- frontend/src/components/EditRecipe/EditRecipeForm.js
+++ frontend/src/components/EditRecipe/EditRecipeForm.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import { withRouter } from 'react-router-dom';
 import Select from 'react-select';
 import TagsBar from '../TagsBar/TagsBar';

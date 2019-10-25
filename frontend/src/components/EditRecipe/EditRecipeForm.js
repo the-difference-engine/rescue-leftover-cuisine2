18dd899683396a5e878786cca75be5b70c7d30db
Rimma <rim23ma@gmail.com>
2019-10-24 19:35:39 -0500

    merge with staging

--- frontend/src/components/EditRecipe/EditRecipeForm.js
+++ frontend/src/components/EditRecipe/EditRecipeForm.js
@@ -27,70 +27,65 @@ const EditRecipeForm = ({ history }) => {
       <div className="row form-recipe-label">
         <div className="form-title col-4 offset-4">
           <label className="detail-labels" htmlFor="title">
-            Recipe Title
+          Recipe Title
             <input className="form-control input-sm recipe-details" id="title" type="text" name="title" value={title} onChange={e => setTitle(e.target.value)} />
           </label>
         </div>
         <div className="form-snippet col-6 offset-3">
           <label className="detail-labels" htmlFor="snippet">
-            Recipe Description
+          Recipe Description
             <textarea className="form-control recipe-details" id="description" name="description" value={description} rows="4" onChange={e => setDescription(e.target.value)} />
           </label>
         </div>
-      </div>
-      <div className="search-tag offset-4">
-        <h4> Add tag </h4>
-        <SearchTag />
-      </div>
-      <div className="row form-dropdown">
-        <div className="dropdown col-2 offset-3">
-          <img src="https://img.icons8.com/material/26/000000/bar-chart.png" alt="difficulty" />
-          <div className="row">
-            <div className="dropdown col-2 offset-3 form-dropdown ">
-              <h2 className="dropdown-headings">
-                <img className="recipe-detail-icons" src={barChart} alt="difficulty" />
-                Difficulty
-              </h2>
-              <select className="recipe-details detail-selection" name="difficulty" value={difficulty} onChange={e => setDifficulty(e.target.value)}>
-                <option>Advanced</option>
-                <option>Medium</option>
-                <option>Easy</option>
-              </select>
-            </div>
-            <div className="dropdown col-2 form-dropdown">
-              <h2 className="dropdown-headings">
-                <i className="recipe-detail-icons fas fa-clock" />
-                Duration
-              </h2>
-              <select className="recipe-details detail-selection" name="duration" value={duration} onChange={e => setDuration(e.target.value)}>
-                <option>30 minutes</option>
-                <option>60 minutes</option>
-                <option>90 minutes</option>
-                <option>120 minutes</option>
-              </select>
-            </div>
-            <div className="dropdown col-2 form-dropdown">
-              <h2 className="dropdown-headings">
-                <i className="recipe-detail-icons fas fa-utensil-spoon" />
-                Servings
-              </h2>
-              <select className="recipe-details detail-selection" name="servings" value={servings} onChange={e => setServings(e.target.value)}>
-                <option>2</option>
-                <option>4</option>
-                <option>6</option>
-                <option>8</option>
-                <option>10</option>
-                <option>12</option>
-              </select>
-            </div>
-          </div>
-          <div id="recipe-submit-containaner">
-            <button type="submit" id="recipe-submit-btn" value="submit" onClick={handleSubmit}>Submit</button>
-          </div>
-          <div className="row form-footer">
-            <Footer />
-          </div>
+        <div className="search-tag offset-4">
+          <h4> Add tag </h4>
+          <SearchTag />
         </div>
+      </div>
+      <div className="row">
+        <div className="dropdown col-2 offset-3 form-dropdown ">
+          <h2 className="dropdown-headings">
+            <img className="recipe-detail-icons" src={barChart} alt="difficulty" />
+          Difficulty
+          </h2>
+          <select className="recipe-details detail-selection" name="difficulty" value={difficulty} onChange={e => setDifficulty(e.target.value)}>
+            <option>Advanced</option>
+            <option>Medium</option>
+            <option>Easy</option>
+          </select>
+        </div>
+        <div className="dropdown col-2 form-dropdown">
+          <h2 className="dropdown-headings">
+            <i className="recipe-detail-icons fas fa-clock" />
+          Duration
+          </h2>
+          <select className="recipe-details detail-selection" name="duration" value={duration} onChange={e => setDuration(e.target.value)}>
+            <option>30 minutes</option>
+            <option>60 minutes</option>
+            <option>90 minutes</option>
+            <option>120 minutes</option>
+          </select>
+        </div>
+        <div className="dropdown col-2 form-dropdown">
+          <h2 className="dropdown-headings">
+            <i className="recipe-detail-icons fas fa-utensil-spoon" />
+          Servings
+          </h2>
+          <select className="recipe-details detail-selection" name="servings" value={servings} onChange={e => setServings(e.target.value)}>
+            <option>2</option>
+            <option>4</option>
+            <option>6</option>
+            <option>8</option>
+            <option>10</option>
+            <option>12</option>
+          </select>
+        </div>
+      </div>
+      <div id="recipe-submit-containaner">
+        <button type="submit" id="recipe-submit-btn" value="submit" onClick={handleSubmit}>Submit</button>
+      </div>
+      <div className="row form-footer">
+        <Footer />
       </div>
     </div>
   );

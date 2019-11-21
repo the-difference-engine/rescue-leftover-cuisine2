721dba46e72b2acd6a87eb370b107039fc3b2fb9
Rimma <rim23ma@gmail.com>
2019-11-20 19:21:47 -0600

    deleted dummy data for recipes at ProfilePage

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,39 +1,41 @@
 class Api::V1::RecipesController < ApplicationController
   before_action :authenticate_user!, :except => [:show, :index]
 
-  def show
-    @recipe = Recipe.find_by!(id: params[:id])
-    render json: @recipe, :include => [{comments: {include: :user }}, :tags]
-  end
+    def show
+      @recipe = Recipe.find_by!(id: params[:id])
+      render json: @recipe, :include => [{comments: {include: :user }}, :tags]
+    end
 
-  def index
-    if params[:search]
-      @recipes = Recipe.search(params[:search])
-    else
-      if params[:user_id]
-      @recipes = Recipe.where(user_id: params[:user_id]).all
+    def index
+      if params[:search]
+        @recipes = Recipe.search(params[:search])
       else
-      @recipes = Recipe.includes(:user).all
-    end
-    render json: @recipes, :include => [:user]
-  end
-
-  def create
-    rp = recipe_params
-    tags = rp.delete(:tags)
-
-    @recipe = Recipe.new(rp)
-    tags.each do |tag|
-      @recipe.tags << Tag.find(tag[:id])
+        if params[:user_id]
+        @recipes = Recipe.where(user_id: params[:user_id]).all
+        else
+        @recipes = Recipe.includes(:user).all
+      end
+      render json: @recipes, :include => [:user]
     end
 
-    @recipe.user = current_user
-    @recipe.save
-    render json: @recipe
-  end
+    def create
+      rp = recipe_params
+      tags = rp.delete(:tags)
 
-  def recipe_params
-   params.require(:recipe).permit(:title, :snippet, :difficulty, :duration, :servings, ingredients: [], tags: [:id, :title], directions: [])
+      @recipe = Recipe.new(rp)
+      tags.each do |tag|
+        @recipe.tags << Tag.find(tag[:id])
+      end
+
+      @recipe.user = current_user
+      @recipe.save
+      render json: @recipe
+    end
+
+    def recipe_params
+    params.require(:recipe).permit(:title, :snippet, :difficulty, :duration, :servings, ingredients: [], tags: [:id, :title], directions: [])
+    end
+
   end
 
 end
--- frontend/src/containers/ProfilePage/ProfilePage.js
+++ frontend/src/containers/ProfilePage/ProfilePage.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   TabContent, TabPane, Nav, NavItem, NavLink,
 } from 'reactstrap';
@@ -11,73 +11,18 @@ import Footer from '../../components/Footer/Footer';
 import RecipeCard from '../../components/RecipeCard/RecipeCard';
 import UserInfo from '../../components/UserProfilePanels/UserInfo';
 import SettingsTab from '../../components/UserProfilePanels/SettingsTab';
+import { getUserRecipes } from '../../lib/apiClient';
 import './ProfilePage.css';
 
 const ProfilePage = ({
   user, setUser, setJwt, history, location,
 }) => {
-  const recipes = [
-    {
-      id: 1,
-      title: 'Cauliflower Salad',
-      snippet: 'Soul-satisfying despite its healthy ingredients, this salad sings',
-      meal: 'DINNER',
-      ingredients: [
-        '1 head cauliflower',
-        '2 Tbsp olive oil',
-        '1 can oil packaged tuna',
-        '2 tsp Dijon mustard',
-        'Juice of 2 lemons',
-        'Salt and pepper',
-      ],
-      directions: [
-        'Roast the cauliflower',
-        'Make the dressing',
-        'Toss',
-      ],
-      difficulty: 'EASY',
-      duration: 60,
-      servings: 6,
-      photos: [
-        '/recipe-seeds/cauliflowersalad.jpg',
-      ],
-      created_at: '2019-05-02T00:21:41.113Z',
-      updated_at: '2019-05-02T00:21:41.113Z',
-      user_id: 1,
-    },
-    {
-      id: 3,
-      title: 'Deviled Eggs',
-      snippet: 'Everyone\'s favorite party snack!',
-      meal: 'SNACK',
-      ingredients: [
-        '12 eggs',
-        '2 Tbsp mustard',
-        '3 Tbsp mayonnaise',
-        '1 tsp paprika',
-        '6 cornichons, diced',
-        'Chives',
-      ],
-      directions: [
-        'Boil the eggs',
-        'Slice in half an remove yolks',
-        'Mix yolks with other ingredients except chives',
-        'Fill eggs with yolk mixture, garnish with paprika and chives',
-      ],
-      difficulty: 'MEDIUM',
-      duration: 30,
-      servings: 4,
-      photos: [
-        '/recipe-seeds/deviledeggs.jpg',
-      ],
-      created_at: '2019-05-02T00:21:41.142Z',
-      updated_at: '2019-05-02T00:21:41.142Z',
-      user_id: 1,
-    },
-  ];
+  const [recipes, setRecipes] = useState([]);
 
-  useEffect(() => {
+  useEffect(async () => {
     window.scrollTo(0, 0);
+    const recipesData = await getUserRecipes(user.id);
+    setRecipes(recipesData);
   }, []);
 
   let activeTab = '';

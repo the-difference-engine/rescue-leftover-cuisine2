86af6803cf9ff425720a52a587c834407587f95e
Rimma <rim23ma@gmail.com>
2019-11-21 18:26:08 -0600

    changed indentation in recipeController and used elsif instead of else if

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,41 +1,38 @@
 class Api::V1::RecipesController < ApplicationController
-  before_action :authenticate_user!, :except => [:show, :index]
+before_action :authenticate_user!, :except => [:show, :index]
 
-    def show
-      @recipe = Recipe.find_by!(id: params[:id])
-      render json: @recipe, :include => [{comments: {include: :user }}, :tags]
-    end
+  def show
+    @recipe = Recipe.find_by!(id: params[:id])
+    render json: @recipe, :include => [{comments: {include: :user }}, :tags]
+  end
 
-    def index
-      if params[:search]
-        @recipes = Recipe.search(params[:search])
+  def index
+    if params[:search]
+      @recipes = Recipe.search(params[:search])
+    elsif params[:user_id]
+      @recipes = Recipe.where(user_id: params[:user_id]).all
       else
-        if params[:user_id]
-        @recipes = Recipe.where(user_id: params[:user_id]).all
-        else
-        @recipes = Recipe.includes(:user).all
-      end
-      render json: @recipes.all.order("created_at DESC"), :include => [:user]
+      @recipes = Recipe.includes(:user).all
+    end
+    render json: @recipes.all.order("created_at DESC"), :include => [:user]
+  end
+
+  def create
+    rp = recipe_params
+    tags = rp.delete(:tags)
+
+    @recipe = Recipe.new(rp)
+    tags.each do |tag|
+      @recipe.tags << Tag.find(tag[:id])
     end
 
-    def create
-      rp = recipe_params
-      tags = rp.delete(:tags)
+    @recipe.user = current_user
+    @recipe.save
+    render json: @recipe
+  end
 
-      @recipe = Recipe.new(rp)
-      tags.each do |tag|
-        @recipe.tags << Tag.find(tag[:id])
-      end
-
-      @recipe.user = current_user
-      @recipe.save
-      render json: @recipe
-    end
-
-    def recipe_params
-    params.require(:recipe).permit(:title, :snippet, :difficulty, :duration, :servings, ingredients: [], tags: [:id, :title], directions: [])
-    end
-
+  def recipe_params
+  params.require(:recipe).permit(:title, :snippet, :difficulty, :duration, :servings, ingredients: [], tags: [:id, :title], directions: [])
   end
 
 end

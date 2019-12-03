410179c31c849c96edaab1ad1a29a5a286ac6445
Rimma <rim23ma@gmail.com>
2019-12-02 20:21:46 -0600

    changed text

--- frontend/src/containers/Page404/Page404.js
+++ frontend/src/containers/Page404/Page404.js
@@ -2,12 +2,11 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 import './Page404.css';
 
-const Page404 = ({ location }) => (
+const Page404 = () => (
   <div id="error">
     <h1 className="notFoundTitle">404</h1>
     <h2 className="notFoundText">
-      No match found for &nbsp;
-      <code>{location.pathname}</code>
+    The page you are looking for cannot be found.
     </h2>
   </div>
 );

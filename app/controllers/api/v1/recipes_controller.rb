bfaf9078f82d7dceca623456800b49381558450f
Rimma <rim23ma@gmail.com>
2019-12-18 19:55:50 -0600

    limit 20 recipes per page

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -13,7 +13,7 @@ class Api::V1::RecipesController < ApplicationController
     elsif params[:user_id]
       @recipes = Recipe.where(user_id: params[:user_id]).all
     else
-      @recipes = Recipe.includes(:user).paginate(:page => params[:page], :per_page => 1)
+      @recipes = Recipe.includes(:user).paginate(:page => params[:page], :per_page => 20)
     end
     render json: @recipes.all.order("created_at DESC"), :include => [:user]
   end
